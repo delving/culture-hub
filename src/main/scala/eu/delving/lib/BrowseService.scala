1ba5215832eb0cd7fd3c6d386f2a4e6f8e8dfaaf
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-06-16 17:30:17 +0200

    Hook for serving KML

--- src/main/scala/eu/delving/lib/BrowseService.scala
+++ src/main/scala/eu/delving/lib/BrowseService.scala
@@ -1,9 +1,9 @@
 package eu.delving.lib
 
 import eu.delving.model._
-import net.liftweb.http.rest.{JsonXmlAble, RestHelper}
 import net.liftweb.common._
-import net.liftweb.http.Req
+import net.liftweb.http.rest.{JsonXmlAble, RestHelper}
+import net.liftweb.http.{PlainTextResponse, Req}
 
 /**
  * Dispatch the services
@@ -21,9 +21,19 @@ object BrowseService extends RestHelper {
   override protected def jsonResponse_?(in: Req): Boolean = (in.acceptsJson_? && !acceptsHtml(in)) || in.param("format") == "json"
   override protected def xmlResponse_?(in: Req): Boolean = (in.acceptsXml_? && !acceptsHtml(in)) || in.param("format") == "xml"
 
+  protected trait KmlTest {
+    def testResponse_?(r: Req): Boolean = r.param("format") == "kml" || r.weightedAccept.find(_.matches("application" -> "vnd.google-earth.kml+xml")).isDefined
+  }
+
+  protected lazy val KmlGet = new TestGet with KmlTest
+
   case class Response(response: AnyRef) extends JsonXmlAble
 
   def give(any : AnyRef) = Full(Response(any))
+
+  serve {
+    case "service" :: "user" :: Nil KmlGet _ => PlainTextResponse("This is some KML")
+  }
 
   serveJx[Response] {
     case "service" :: "user" :: Nil Get _ => give(User.findAll.head.getValue)
