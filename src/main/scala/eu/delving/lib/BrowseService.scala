baa6e2083f1b9b11a334cefaefaf83fd330b945c
Gerald de Jong <gerald@delving.eu>
2011-06-16 10:12:11 +0200

    Created initial BrowseService

---
+++ src/main/scala/eu/delving/lib/BrowseService.scala
@@ -0,0 +1,19 @@
+package eu.delving.lib
+
+import net.liftweb.http.rest.RestHelper
+import net.liftweb.common._
+/**
+ * Dispatch the services
+ *
+ * @author Gerald de Jong <geralddejong@gmail.com>
+ */
+
+
+object BrowseService extends RestHelper {
+
+  val log = Logger("BrowseService")
+
+  serveJx {
+  }
+
+}
