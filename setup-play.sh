9fdc026a09d4d53afaa314aec21900e9b3c2db8c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-06-29 15:23:56 +0200

    Installation script for play

---
+++ setup-play.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo ===================================
+echo Installing play in ../play-1.2.2RC2
+echo ===================================
+echo
+echo
+
+cd ..
+wget http://download.playframework.org/releases/play-1.2.2RC2.zip
+unzip play-1.2.2RC2.zip
+cd play-1.2.2RC2
+
+echo
+echo
+echo ========================
+echo Installing play-scala...
+echo ========================
+echo
+echo
+
+play install scala-0.9.1
+
+cd ..
+
+echo ================================================================
+echo Done! Add play-1.2.2RC2 to your path in order to run play easily
+echo ================================================================
