041cd24e6de515070834b9e414f0d6dce2a9776a
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-01 13:11:44 +0200

    Experimental support for automatic multi-format rendering. The syntax needs to be improved, but the basic mechanism is there

--- app/controllers/Profiles.scala
+++ app/controllers/Profiles.scala
@@ -6,41 +6,20 @@ import net.liftweb.json.Serialization.{read, write}
 import play.classloading.enhancers.LocalvariablesNamesEnhancer
 import java.lang.reflect.Constructor
 import net.liftweb.json.{Extraction, DefaultFormats, ParameterNameReader}
+import extensions.AdditionalActions
 
 /**
  *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 
-// glue for lift-json
-object PlayParameterNameReader extends ParameterNameReader {
-  def lookupParameterNames(constructor: Constructor[_]) =  {
-    Set(LocalvariablesNamesEnhancer.lookupParameterNames(constructor).toArray(new Array[String](1)) : _*)
-  }
-}
-
-object Profiles extends Controller {
+object Profiles extends Controller with AdditionalActions {
 
   import views.Profile._
 
-  implicit val formats = new DefaultFormats {
-    override val parameterNameReader = PlayParameterNameReader
-  }
-
-
   def index(): AnyRef = {
-
     val displayName = params.get("user")
-
-    val user: User = User.find("displayName = {displayName}").on("displayName" -> displayName).first().getOrElse(User.nobody)
-
-    if (request.format == "json") {
-      return Json(write(user))
-    }
-    if(request.format == "xml") {
-      return Xml(<response>{net.liftweb.json.Xml.toXml(Extraction.decompose(user))}</response>)
-    }
-
-    html.index(user = user)
+    val u: User = User.find("displayName = {displayName}").on("displayName" -> displayName).first().getOrElse(User.nobody)
+    RenderMultitype(html.index, ('user, u))
   }
 }
---
+++ app/extensions/AdditionalActions.scala
@@ -0,0 +1,67 @@
+package extensions
+
+import play.classloading.enhancers.LocalvariablesNamesEnhancer
+import play.mvc.Http.{Response, Request}
+import net.liftweb.json.Serialization._
+import net.liftweb.json.{Extraction, DefaultFormats, ParameterNameReader}
+import views.Profile.html
+import reflect.Template
+import play.mvc.ScalaControllerCompatibility
+import java.lang.reflect.{Method, Constructor}
+import play.templates.Html
+import play.mvc.results.{RenderHtml, RenderXml, RenderJson, Result}
+
+/**
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+
+// glue for lift-json
+object PlayParameterNameReader extends ParameterNameReader {
+  def lookupParameterNames(constructor: Constructor[_]) = {
+    Set(LocalvariablesNamesEnhancer.lookupParameterNames(constructor).toArray(new Array[String](1)): _*)
+  }
+}
+
+/**
+ * This trait provides additional actions that can be used in controllers
+ */
+trait AdditionalActions {
+
+  def RenderMultitype(template: play.templates.BaseScalaTemplate[play.templates.Html, play.templates.Format[play.templates.Html]], args: (Symbol, Any)*) = new RenderMultitype(template, args: _*)
+
+}
+
+/**
+ * Experimental action to generically render an object as json, xml or html depending on the requested format.
+ */
+class RenderMultitype(template: play.templates.BaseScalaTemplate[play.templates.Html, play.templates.Format[play.templates.Html]], args: (Symbol, Any)*) extends Result {
+
+  def apply(request: Request, response: Response) {
+
+    implicit val formats = new DefaultFormats {
+      override val parameterNameReader = PlayParameterNameReader
+    }
+
+    // TODO we for the moment only handle a single parameter. We have to see how multi-parameter JSON and XML responses would look like
+    val arg = args(0)._2.asInstanceOf[AnyRef]
+
+    if (request.format == "json") {
+      new RenderJson(write(arg))(request, response)
+    } else if (request.format == "xml") {
+      new RenderXml(<response>
+        {net.liftweb.json.Xml.toXml(Extraction.decompose(arg))}
+      </response>)(request, response)
+    } else {
+      // TODO this was hacked together in five minutes. Due to lack of knowledge of the scala reflection mechnism I resolved to the ugly code below
+      // but I guess there is a better way
+      val c = Class.forName(template.getClass.getName)
+      val templateObject: AnyRef = c.getField("MODULE$").get(null).asInstanceOf[template.type]
+      val method: Method = templateObject.getClass.getMethod("apply", arg.asInstanceOf[AnyRef].getClass)
+      val invoked: Html = method.invoke(template, arg.asInstanceOf[AnyRef]).asInstanceOf[Html]
+
+      new RenderHtml(invoked.toString())(request, response)
+    }
+  }
+
+}
