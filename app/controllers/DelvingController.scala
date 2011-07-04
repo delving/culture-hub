3b8c07641bb0da6da951de80586e98ca96646233
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-04 13:31:01 +0200

    Porting the forbidden keyword list from lift-services

--- app/controllers/DelvingController.scala
+++ app/controllers/DelvingController.scala
@@ -1,20 +1,55 @@
 package controllers
 
 import _root_.models.User
-import play.mvc.Controller
 import extensions.AdditionalActions
+import play.mvc.{Before, Controller}
 
 /**
  * Root controller for culture-hub. Takes care of checking URL parameters and other generic concerns.
- * 
+ *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ * @author Gerald de Jong <geralddejong@gmail.com>
  */
 
-class DelvingController extends Controller with AdditionalActions {
+trait DelvingController extends Controller with AdditionalActions {
+
+  @Before
+  def checkParameters(): AnyRef = {
+    val parametersToCheck = List("user", "collection", "label", "dobject", "story")
+    parametersToCheck map {
+      paramName => Option(params.get(paramName)) map {
+        paramValue => Allowed(paramValue) match {
+          case None => return Error("""Forbidden value "%s" for parameter "%s"""" format (paramValue, paramName))
+          case _ => Continue
+        }
+      }
+    }
+    Continue
+  }
+
+  object Allowed {
+    def apply(string: String): Option[String] = {
+      FORBIDDEN map {
+        f => if (string.contains(f)) {
+          return None
+        }
+      }
+      Some(string)
+    }
+  }
+
+  val FORBIDDEN = Set(
+    "object", "profile", "map", "graph", "label", "collection",
+    "story", "user", "service", "services", "portal", "api", "index",
+    "add", "edit", "save", "delete", "update", "create", "search",
+    "image", "fcgi-bin", "upload")
+
 
   def getUser(displayName: String): User = {
     User.find("displayName = {displayName}").on("displayName" -> displayName).first().getOrElse(User.nobody)
   }
+}
 
+object DelvingController {
 
 }
