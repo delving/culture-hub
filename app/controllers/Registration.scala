6f9c80700c7e3443c8df4e99213e6676a81abcba
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-13 19:42:16 +0200

    Adding captcha to registration

--- app/controllers/Registration.scala
+++ app/controllers/Registration.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.data.validation.{Valid, Validation, Required, Email}
 import models.User
+import play.cache.Cache
+import play.libs.Codec
 
 /**
  *
@@ -12,11 +14,16 @@ object Registration extends DelvingController {
 
   import views.Registration._
 
-  def index() = html.index()
+  def index() = {
+    html.index(randomId = Codec.UUID())
+  }
 
   def register() = {
+    val code = params.get("code")
+    val randomId = params.get("randomID")
+
     Validation.clear()
-    
+
     val r: Registration = params.get("registration", classOf[Registration])
 
     // TODO this should not be done like this, but I haven't figured out yet the way to use the annotation-based way
@@ -27,13 +34,16 @@ object Registration extends DelvingController {
     Validation.required("registration.displayName", r.displayName)
     Validation.required("registration.password1", r.password1)
     Validation.required("registration.password2", r.password2)
-    if(r.password1 != r.password2) {
+    if (r.password1 != r.password2) {
       Validation.addError("registration.password1", "Passwords are not the same", r.password1)
       Validation.addError("registration.password2", "Passwords are not the same", r.password2)
     }
+    Validation.equals("code", code, "code", Cache.get(randomId).orNull).message("Invalid code. Please type it again")
 
     // TODO check email uniqueness
     // TODO check displayName uniqueness
+
+    Cache.delete(randomId)
 
     if (Validation.hasErrors) {
       params.flash()
@@ -49,11 +59,18 @@ object Registration extends DelvingController {
     }
   }
 
+  def captcha(id: String) = {
+    val captcha = play.libs.Images.captcha
+    val code = captcha.getText("#E4EAFD")
+    Cache.set(id, code, "10mn")
+    captcha
+  }
+
   case class Registration(@Required firstName: String,
-                                 @Required lastName: String,
-                                 @Email email: String,
-                                 @Required displayName: String,
-                                 @Required password1: String,
-                                 @Required password2: String)
+                          @Required lastName: String,
+                          @Email email: String,
+                          @Required displayName: String,
+                          @Required password1: String,
+                          @Required password2: String)
 
 }
