c20a251e0494ff62eda870a4b59a236620ce0d21
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-14 15:42:12 +0200

    Mechanism for loading machine-specific properties also in test mode

--- app/bootstrap.scala
+++ app/bootstrap.scala
@@ -1,5 +1,8 @@
+import java.util.Properties
+import java.util.regex.{Pattern, Matcher}
 import play.exceptions.ConfigurationException
 import play.jobs._
+import play.libs.IO
 import play.Play
 
 @OnApplicationStart class BootStrap extends Job {
@@ -8,6 +11,24 @@ import play.Play
 
     import models._
     import play.test._
+    import scala.collection.JavaConversions._
+
+    // also retrieve user-based properties when running in test mode
+    if (Play.id == "test") {
+      val user = Option(System.getProperty("user")).getOrElse(throw new ConfigurationException("When running in test mode, specify the user using the JVM arg '-Duser=melvin'"))
+
+      val pattern: Pattern = Pattern.compile("^%([a-zA-Z0-9_\\-]+)\\.(.*)$")
+      val config: Properties = IO.readUtf8Properties(Play.conf.inputstream())
+      for (key <- config.keySet()) {
+        val matcher: Matcher = pattern.matcher(key + "")
+        if (matcher.matches) {
+          val instance = matcher.group(1)
+          if (instance == user) {
+            Play.configuration.put(matcher.group(2), config.get(key).toString.trim)
+          }
+        }
+      }
+    }
 
     // Check if all required launch properties are there
     Yaml[List[String]]("launchProperties.yml") map {
