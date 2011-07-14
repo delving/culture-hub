5ce1ea413db17800145a8d60ed999a88e999eb3d
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-14 14:18:08 +0200

    Checking for required launch properties, closes #4

--- app/bootstrap.scala
+++ app/bootstrap.scala
@@ -1,4 +1,4 @@
-import cake.ComponentRegistry
+import play.exceptions.ConfigurationException
 import play.jobs._
 import play.Play
 
@@ -9,6 +9,14 @@ import play.Play
     import models._
     import play.test._
 
+    // Check if all required launch properties are there
+    Yaml[List[String]]("launchProperties.yml") map {
+      key =>
+        val value: String = Play.configuration.getProperty(key)
+        if (value == null || value.trim().length() == 0) {
+          throw new ConfigurationException("Required configuration property '" + key + "' is not configured. If you are running in dev mode, make sure you started the framework with 'play run --%youruser'")
+        }
+    }
 
     // Import initial data if the database is empty
     if (User.count() == 0) {
