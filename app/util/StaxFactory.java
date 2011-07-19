24a690075f9bca6a19a609cdade6be1e4f212ab3
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-19 13:43:37 +0200

    Commenting

--- app/util/StaxFactory.java
+++ app/util/StaxFactory.java
@@ -5,6 +5,8 @@ import javax.xml.stream.XMLInputFactory;
 import org.codehaus.stax2.XMLInputFactory2;
 
 /**
+ * Factory class necessary due to Java's (oftentimes criticized) ability to inherit static methods vs. Scala's inability to do so
+ *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 public class StaxFactory {
