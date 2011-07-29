e23fe914a4e85ca84896f5957e9f61468493719e
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-07-29 14:33:36 +0200

    Second attempt with success on lift-json after failing to get sjson to do both serialization AND deserialization including custom object types such as bson ObjectIDs. This may look simple but isn't. The state of json handling in the scala space is seriously wrong, jerkson (https://github.com/codahale/jerkson) seems to do things in a simple way, we may want to look at that later on.

--- app/controllers/user/Admin.scala
+++ app/controllers/user/Admin.scala
@@ -5,14 +5,15 @@ import play.mvc.Before
 import controllers.{UserAuthentication, Secure, DelvingController}
 import play.mvc.results.Result
 import models.UserGroup
-import reflect.BeanInfo
+import extensions.{PlayParameterNameReader, ObjectIdSerializer, LiftJson}
+import net.liftweb.json.{FullTypeHints, Serialization, DefaultFormats}
 
 /**
  *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 
-object Admin extends DelvingController with UserAuthentication with Secure {
+object Admin extends DelvingController with UserAuthentication with Secure with LiftJson {
 
   import views.User.Admin._
 
@@ -36,8 +37,8 @@ object Admin extends DelvingController with UserAuthentication with Secure {
     html.groupNew()
   }
 
-  @BeanInfo case class GroupModel(name: String, readRight: Boolean = false, updateRight: Boolean, deleteRight: Boolean , members: List[Member])
-  @BeanInfo case class Member(id: String, name: String)
+  case class GroupModel(name: String, readRight: Boolean = false, updateRight: Boolean = false, deleteRight: Boolean = false , members: Seq[Member])
+  case class Member(id: String, name: String)
 
 
   def groupLoad(user: String, name: String): Result = {
@@ -47,16 +48,13 @@ object Admin extends DelvingController with UserAuthentication with Secure {
 
   def groupSubmit(user: String, name: String, data: String): Result = {
 
-    import sjson.json._
-    import sjson.json.DefaultProtocol._
-
-    val serializer = Serializer.SJSON
-
     println(data)
+    println(in[GroupModel](data))
 
-    val model: GroupModel = serializer.in[GroupModel](data)
-
+    val model: GroupModel = in[GroupModel](data)
     println(model)
+
+    println(out[GroupModel](model))
 
     Ok
   }
