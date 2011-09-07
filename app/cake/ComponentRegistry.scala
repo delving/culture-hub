46959691b585d9075d15e09b397d79c75d8a1871
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-09-07 16:51:45 +0200

    Better error reporting on ComponentRegistry initialization failure

--- app/cake/ComponentRegistry.scala
+++ app/cake/ComponentRegistry.scala
@@ -2,6 +2,12 @@ package cake
 
 import extensions.{ObjectIdTypeBinder, ScalaListTypeBinder}
 import util.{ThemeHandler, ThemeHandlerComponent}
+import scala.collection.JavaConversions._
+import play.Play
+import eu.delving.sip.AccessKey
+import eu.delving.metadata.MetadataModel
+import eu.delving.metadata.MetadataModelImpl
+
 
 /**
  * This object uses the Cake pattern to manage Dependency Injection, see also
@@ -32,31 +38,32 @@ trait MetaRepoComponent {
 // instantiate the services in a module
 object ComponentRegistry extends MetadataModelComponent with ThemeHandlerComponent with MetaRepoComponent {
 
-  import scala.collection.JavaConversions._
-  import play.Play
-  import eu.delving.sip.AccessKey
-  import eu.delving.metadata.MetadataModel
-  import eu.delving.metadata.MetadataModelImpl
-
   val metadataModel: MetadataModel = new MetadataModelImpl
-  metadataModel.asInstanceOf[MetadataModelImpl].setDefaultPrefix(Play.configuration.getProperty("services.harvindexing.prefix"))
-  metadataModel.asInstanceOf[MetadataModelImpl].setRecordDefinitionResources(List("/abm-record-definition.xml", "/icn-record-definition.xml", "/ese-record-definition.xml"))
-
   val accessKey = new AccessKey
-  accessKey.setServicesPassword(Play.configuration.getProperty("services.password").trim)
+  val themeHandler: ThemeHandler = new ThemeHandler
 
   //  val metaRepo = new MetaRepoImpl
   //  metaRepo.setResponseListSize(Play.configuration.getProperty("services.pmh.responseListSize").trim)
   //  metaRepo.setHarvestStepSecondsToLive(180)
 
-  // FIXME this way of initializing things does not allow for a very good exception handling. if something goes wrong in startup we get an ClassNotDefFound in clients of the ComponentRegistry
-  val themeHandler: ThemeHandler = new ThemeHandler
-  try {
-    themeHandler.startup()
-  } catch {
-    case t: Throwable => t.printStackTrace()
-  }
-
+  // TODO remove this when we have replaced the old code for theme handling
   play.data.binding.Binder.register(classOf[scala.collection.immutable.List[String]], new ScalaListTypeBinder)
   play.data.binding.Binder.register(classOf[org.bson.types.ObjectId], new ObjectIdTypeBinder)
+
+  init()
+
+  def init() {
+    try {
+      metadataModel.asInstanceOf[MetadataModelImpl].setDefaultPrefix(Play.configuration.getProperty("services.harvindexing.prefix"))
+      metadataModel.asInstanceOf[MetadataModelImpl].setRecordDefinitionResources(List("/abm-record-definition.xml", "/icn-record-definition.xml", "/ese-record-definition.xml"))
+
+      accessKey.setServicesPassword(Play.configuration.getProperty("services.password").trim)
+
+      themeHandler.startup()
+    } catch {
+      case t: Throwable => t.printStackTrace()
+    }
+
+  }
+
 }
