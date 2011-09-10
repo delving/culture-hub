267b0bfa73d0cdf81fd6c487c497cdcac4b83c2f
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-09-10 10:53:39 +0200

    Pagination plugin, starting integration with object list page

--- app/controllers/Dobjects.scala
+++ app/controllers/Dobjects.scala
@@ -15,14 +15,14 @@ object DObjects extends DelvingController {
 
   import views.Dobject._
 
-  def list(user: String): AnyRef = {
+  def list(user: String, query: String, page: Int = 1): AnyRef = {
     val u = getUser(user)
 
     // TODO access rights
     val objects = Map("availableObjects" -> DObject.findByUser(browsedUserId).map {o => ObjectModel(Some(o._id), o.name, o.description, o.user_id)})
 
     request.format match {
-      case "html" => html.list(user = u)
+      case "html" => html.list(user = u, page = page, count = 58)
       case "json" => Json(objects)
     }
   }
