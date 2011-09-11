0f3c83e99b12d8a8bcfbcfacc86eda4afb158701
zemyatin <eric@delving.eu>
2011-09-11 15:13:50 +0200

    New and improved user navigation!

--- public/themes/common/javascripts/culturehub.js
+++ public/themes/common/javascripts/culturehub.js
@@ -4,30 +4,87 @@
 function initializeElements() {
     $(".saveButton").button();
 
-    //all hover and click logic for dui-buttons
-    $(".dui-button:not(.ui-state-disabled)")
-            .hover(
-            function() {
-                $(this).addClass("ui-state-hover");
-            },
-            function() {
-                $(this).removeClass("ui-state-hover");
-            }
-    )
-            .mousedown(function() {
-                $(this).parents('.dui-buttonset-single:first').find(".dui-button.ui-state-active").removeClass("ui-state-active");
-                if ($(this).is('.ui-state-active.dui-button-toggleable, .dui-buttonset-multi .ui-state-active')) {
-                    $(this).removeClass("ui-state-active");
-                }
-                else {
-                    $(this).addClass("ui-state-active");
-                }
-            })
-            .mouseup(function() {
-                if (! $(this).is('.dui-button-toggleable, .dui-buttonset-single .dui-button,  .dui-buttonset-multi .dui-button')) {
-                    $(this).removeClass("ui-state-active");
-                }
+//    //all hover and click logic for dui-buttons
+//    $(".dui-button:not(.ui-state-disabled)")
+//            .hover(
+//            function() {
+//                $(this).addClass("ui-state-hover");
+//            },
+//            function() {
+//                $(this).removeClass("ui-state-hover");
+//            }
+//    )
+//            .mousedown(function() {
+//                $(this).parents('.dui-buttonset-single:first').find(".dui-button.ui-state-active").removeClass("ui-state-active");
+//                if ($(this).is('.ui-state-active.dui-button-toggleable, .dui-buttonset-multi .ui-state-active')) {
+//                    $(this).removeClass("ui-state-active");
+//                }
+//                else {
+//                    $(this).addClass("ui-state-active");
+//                }
+//            })
+//            .mouseup(function() {
+//                if (! $(this).is('.dui-button-toggleable, .dui-buttonset-single .dui-button,  .dui-buttonset-multi .dui-button')) {
+//                    $(this).removeClass("ui-state-active");
+//                }
+//            });
+
+//On Hover Over
+function megaHoverOver(){
+    $(this).find(".sub").stop().fadeTo('fast', 1).show(); //Find sub and fade it in
+    (function($) {
+        //Function to calculate total width of all ul's
+        jQuery.fn.calcSubWidth = function() {
+            rowWidth = 0;
+            //Calculate row
+            $(this).find("ul").each(function() { //for each ul...
+                rowWidth += $(this).width(); //Add each ul's width together
             });
+        };
+    })(jQuery);
+
+    if ( $(this).find(".row").length > 0 ) { //If row exists...
+
+        var biggestRow = 0;
+
+        $(this).find(".row").each(function() {	//for each row...
+            $(this).calcSubWidth(); //Call function to calculate width of all ul's
+            //Find biggest row
+            if(rowWidth > biggestRow) {
+                biggestRow = rowWidth;
+            }
+        });
+
+        $(this).find(".sub").css({'width' :biggestRow}); //Set width
+        $(this).find(".row:last").css({'margin':'0'});  //Kill last row's margin
+
+    } else { //If row does not exist...
+
+        $(this).calcSubWidth();  //Call function to calculate width of all ul's
+        $(this).find(".sub").css({'width' : rowWidth}); //Set Width
+
+    }
+}
+//On Hover Out
+function megaHoverOut(){
+  $(this).find(".sub").stop().fadeTo('fast', 0, function() { //Fade to 0 opactiy
+      $(this).hide();  //after fading, hide it
+  });
+}
+
+//Set custom configurations
+var config = {
+     sensitivity: 2, // number = sensitivity threshold (must be 1 or higher)
+     interval: 100, // number = milliseconds for onMouseOver polling interval
+     over: megaHoverOver, // function = onMouseOver callback (REQUIRED)
+     timeout: 500, // number = milliseconds delay before onMouseOut
+     out: megaHoverOut // function = onMouseOut callback (REQUIRED)
+};
+
+$("ul#user-menu li .sub").css({'opacity':'0'}); //Fade sub nav to 0 opacity on default
+$("ul#user-menu li").hoverIntent(config); //Trigger Hover intent with custom configurations
+
+
 }
 
 /**
