64a401ec3acd72dd5c88d1ea765e8d9c246678a8
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-09-11 15:43:34 +0200

    Really fixing the pagination count

--- app/models/Commons.scala
+++ app/models/Commons.scala
@@ -28,13 +28,13 @@ trait Pager[A <: salat.CaseClass] { self: AnyRef with SalatDAO[A, ObjectId] =>
   implicit def cursorWithPage(cursor: SalatMongoCursor[A]) = new {
 
     /**
-     * Returns a page and the total page count
+     * Returns a page and the total object count
      * @param page the page number
      * @param pageSize optional size of the page, defaults to 20
      */
     def page(page: Int, pageSize: Int = 20) = {
       val c = cursor.skip((page - 1) * pageSize).limit(20)
-      (c.toList, (c.count / pageSize.toDouble).ceil.toInt)
+      (c.toList, c.count)
     }
   }
 }
