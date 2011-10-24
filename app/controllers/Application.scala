f37479752ea66f032f94459c27727a58d8a9da6f
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-10-24 16:29:56 +0200

    API for fetching theme properties stored in application.conf. This is first rudimentary implementation, the only data-types that are supported are (single) strings and ints.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,9 +12,9 @@ object Application extends DelvingController {
 
   def index: Result = {
 
-    val recentCollections: List[ShortCollection] = UserCollection.findRecent(3).toList
-    val recentStories: List[ShortStory] = Story.findRecent(3).toList
-    val recentObjects: List[ListItem] = DObject.findRecent(12).toList
+    val recentCollections: List[ShortCollection] = UserCollection.findRecent(viewUtils.themeProperty("recentCollectionsCount", classOf[Int])).toList
+    val recentStories: List[ShortStory] = Story.findRecent(viewUtils.themeProperty("recentStoriesCount", classOf[Int])).toList
+    val recentObjects: List[ListItem] = DObject.findRecent(viewUtils.themeProperty("recentObjectsCount", classOf[Int])).toList
 
     Template('recentCollections -> recentCollections, 'recentStories -> recentStories, 'recentObjects -> recentObjects)
   }
