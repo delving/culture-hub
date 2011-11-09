08d3fbc4647522dc838aba3c426982fcfbe14613
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-11-09 17:22:52 +0100

    Controller for organization user view

---
+++ app/controllers/organization/Organization.scala
@@ -0,0 +1,14 @@
+package controllers.organization
+
+import controllers.DelvingController
+
+/**
+ * Public Organization controller
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+
+object Organization extends DelvingController {
+
+  def index = Template
+
+}
