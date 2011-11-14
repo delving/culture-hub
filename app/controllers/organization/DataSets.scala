5c4e228c93a5ad0cd723e3fa98d671882a6c07ed
zemyatin <eric@delving.eu>
2011-11-14 18:02:27 +0100

    search controller update to get query params, dataset list ordered by spec

--- app/controllers/organization/DataSets.scala
+++ app/controllers/organization/DataSets.scala
@@ -5,7 +5,7 @@ import play.mvc.results.Result
 import com.mongodb.casbah.commons.MongoDBObject
 import collection.JavaConversions._
 import controllers.{Fact, ShortDataSet, Token, DelvingController}
-import models.{GrantType, Organization, DataSet}
+import models.{Organization, DataSet}
 
 /**
  *
@@ -15,17 +15,28 @@ import models.{GrantType, Organization, DataSet}
 object DataSets extends DelvingController with OrganizationSecured {
 
   def list(orgId: String, page: Int = 1): Result = {
-    val dataSetsPage = DataSet.findAllCanSee(orgId, connectedUser).page(page)
+    val dataSetsPage = DataSet.findAllByOrgId(orgId).page(page)
     val items: List[ShortDataSet] = dataSetsPage._1
-    Template('title -> listPageTitle("dataset"), 'items -> items, 'page -> page, 'count -> dataSetsPage._2, 'isOwner -> Organization.isOwner(orgId, connectedUser))
+    Template('title -> listPageTitle("dataset"), 'items -> items.sortBy(_.spec), 'page -> page, 'count -> dataSetsPage._2, 'isOwner -> Organization.isOwner(connectedUser))
   }
 
   def dataSet(orgId: String, spec: String): Result = {
+    // TODO check if connected user has access
     val dataSet = DataSet.findBySpecAndOrgId(spec, orgId)
-    val ds = dataSet.getOrElse(return NotFound(&("organization.datasets.dataSetNotFound", spec)))
-    if(!DataSet.canView(ds, connectedUser)) return NotFound(&("datasets.dataSetNotFound", ds.spec))
-    val describedFacts = DataSet.factDefinitionList.map(factDef => Fact(factDef.name, factDef.prompt, Option(ds.details.facts.get(factDef.name)).getOrElse("").toString))
-    Template('dataSet -> ds, 'canEdit -> DataSet.canEdit(ds, connectedUser), 'facts -> asJavaList(describedFacts), 'isOwner -> Organization.isOwner(orgId, connectedUser))
+
+    request.format match {
+      case "html" => {
+        val ds = dataSet.getOrElse(return NotFound(&("organization.datasets.dataSetNotFound", spec)))
+        val describedFacts = DataSet.factDefinitionList.map(factDef => Fact(factDef.name, factDef.prompt, Option(ds.details.facts.get(factDef.name)).getOrElse("").toString))
+        Template('dataSet -> ds, 'facts -> asJavaList(describedFacts))
+      }
+      case "json" => if (dataSet == None) Json(ShortDataSet(userName = connectedUser, orgId = orgId))
+      else {
+        val dS = dataSet.get
+        Json(ShortDataSet(id = Some(dS._id), spec = dS.spec, facts = dS.getFacts, userName = dS.getCreator.userName, orgId = dS.orgId, recordDefinitions = dS.recordDefinitions, visibility = dS.visibility.value))
+      }
+    }
+
   }
 
   def listAsTokens(orgId: String, q: String): Result = {
