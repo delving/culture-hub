9ec119f922208b937a79e32016c2add37df7cd73
zemyatin <eric@delving.eu>
2011-11-16 14:10:01 +0100

    more sass css, story page numbering, renaming of theme templates and includes

--- app/views/package.scala
+++ app/views/package.scala
@@ -40,7 +40,7 @@ package object context extends Internationalization {
   }
 
   def thumbnailUrl(thumbnail: ObjectId) = "/thumbnail/%s/100".format(thumbnail)
-  
+
   def thumbnailUrl(thumbnail: Option[ObjectId]) = thumbnail match {
     case Some(t) => "/thumbnail/%s/100".format(t)
     case None => "/public/images/dummy-object.png" // TODO now that's not very clean, is it?
