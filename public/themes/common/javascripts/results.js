642d24670c3343c0ebc39ceaab3b69b8b03dd1b1
zemyatin <eric@delving.eu>
2011-11-20 15:59:46 +0100

    get addThis functionality to work from within an overlay

--- public/themes/common/javascripts/results.js
+++ public/themes/common/javascripts/results.js
@@ -41,13 +41,18 @@ $(document).ready(function() {
 	// it is assumed to be the onBeforeLoad event listener
 	$("a[rel]").overlay({
 		onBeforeLoad: function() {
-
 			// grab wrapper element inside content
 			var wrap = this.getOverlay().find(".contentWrap");
 
 			// load the page specified in the trigger
 			wrap.load(this.getTrigger().attr("href"));
-		}
-
+		},
+        onLoad: function(){
+            addthis.init();
+        },
+        fixed: false
 	});
+
+
+
 });
