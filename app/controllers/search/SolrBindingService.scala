eed76db3968d98dfb91add4b2cafc207af661ceb
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2011-11-24 21:31:16 +0100

    Small change to the highlighting mechanism

--- app/controllers/search/SolrBindingService.scala
+++ app/controllers/search/SolrBindingService.scala
@@ -107,7 +107,7 @@ object SolrBindingService {
           }
           val id = solrDoc getFirst ("id")
           if (highLightMap != null && highLightMap.containsKey(id)) {
-            highLightMap.get(id).foreach(entry => solrDoc addHighLightField (entry._1, entry._2.toList))
+            highLightMap.get(id).filterNot(_._1.endsWith("_facet")).foreach(entry => solrDoc addHighLightField (entry._1, entry._2.toList))
           }
       docs add solrDoc
     }
--- app/controllers/search/SolrQueryService.scala
+++ app/controllers/search/SolrQueryService.scala
@@ -96,7 +96,7 @@ object SolrQueryService extends SolrServer {
     query setFields ("*,score")
     // highlighting parameters
     query setHighlight true
-    query addHighlightField ("text")
+    query addHighlightField ("*")
   }
 
 //  def getSolrFullItemQueryWithDefaults(facets: List[SolrFacetElement] = List.empty): SolrQuery = {
