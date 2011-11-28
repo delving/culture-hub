89140b255e2ba054b61c59da065ff64567660a2b
zemyatin <eric@delving.eu>
2011-11-28 12:15:09 +0100

    set height per story page according to content height

--- public/themes/common/javascripts/story.js
+++ public/themes/common/javascripts/story.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-    var docHeight = $(document).height();
-    $(".scrollable, .story, .story-container").height(docHeight);
+    var viewAreaHeight = 0;
     var root = $(".scrollable").scrollable().navigator("#pnav").navigator("#inav");
     var api = root.scrollable();
     api.onBeforeSeek(function(event, i) {
         $("#pnav li").removeClass("active").eq(i).addClass("active");
         $("#inav li").removeClass("active").eq(i).addClass("active");
     });
+    api.onSeek(function(event, i) {
+        setHeight();
+    });
+
+    function setHeight() {
+        currentIndex = api.getIndex();
+        $('div#page_' + currentIndex).each(function(i) {
+            viewAreaHeight += $(this).outerHeight() + 20;
+        });
+        $('.scrollable').css("height", viewAreaHeight);
+        viewAreaHeight = 0;
+    }
 });
