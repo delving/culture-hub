fcf51db45ba25735dc7efb45d1a1e8c89f231f1e
zemyatin <eric@delving.eu>
2011-12-07 18:47:59 +0100

    orphan branch reintegration

--- public/common/javascripts/culturehub.js
+++ public/common/javascripts/culturehub.js
@@ -9,7 +9,7 @@ function initializeElements() {
       document.location = document.referrer;
     });
     $.preloadImages (
-        "/public/common/images/spinner.gif"
+        "/public/themes/common/images/spinner.gif"
     );
 }
 
