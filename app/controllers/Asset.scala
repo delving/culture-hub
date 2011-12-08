45acc2895db3ab3e244b5b0c122b5a09e3017075
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-12-08 12:16:30 +0100

    Themes fix: setting content-type correctly, simplifying

--- app/controllers/Asset.scala
+++ app/controllers/Asset.scala
@@ -17,10 +17,10 @@
 package controllers
 
 import play.mvc.Controller
-import java.io.File
-import play.libs.IO
 import play.mvc.results.{RenderBinary, Result}
 import play.{Logger, Play}
+import play.libs.{MimeTypes, IO}
+import java.io.{FileInputStream, File}
 
 /**
  * Helper controller to pre-process assets such as jquery templates
@@ -31,37 +31,39 @@ object Asset extends Controller with Logging with Internationalization {
 
   def get(path: String): Result = {
     val f = new File(Play.applicationPath + path)
-    if(!f.exists()) return NotFound("Asset at path %s not found".format(path))
+    if (!f.exists()) return NotFound("Asset at path %s not found".format(path))
     val content = IO.readContentAsString(f)
     val messages = "\\&\\{([^\\}]*)\\}".r.findAllIn(content).matchData.map(m => (m.group(0), m.group(1))).map {
       m =>
-        val elems: Array[String] = m._2.split(",").map(e => e.trim.substring(1, e.trim.length() -1))
+        val elems: Array[String] = m._2.split(",").map(e => e.trim.substring(1, e.trim.length() - 1))
         val key: String = elems(0)
         val args: Array[String] = elems.slice(1, elems.length)
         (m._1, key, args)
     }
-    Text(messages.foldLeft(content) { (r, c) => r.replace(c._1, &(c._2, c._3 : _*))})
+    Text(messages.foldLeft(content) {
+      (r, c) => r.replace(c._1, &(c._2, c._3: _*))
+    })
   }
 
   def serveTheme(relativePath: String, theme: String): Result = {
-    val available = Play.configuration.getProperty("themes.available", "default").split(",")
-    if(available.contains(theme)) {
-      return Continue
+    val available = new File(Play.applicationPath + "/public/themes/").listFiles().filter(_.isDirectory).map(_.getName)
+    val f = if (available.contains(theme)) {
+      new File(Play.applicationPath + "/public/themes/" + theme + "/" + relativePath)
+    } else {
+      val additionalThemes = Option(Play.configuration.getProperty("themes.additionalThemesDir"))
+      additionalThemes match {
+        case Some(s) =>
+          val f = new File(Play.applicationPath + "/" + s).getCanonicalFile
+          if (!f.exists() || !f.isDirectory) {
+            return Error("Incorrectly configured additional themes directory %s".format(s))
+          }
+          new File(f, theme + "/" + relativePath)
+        case None => return NotFound
+      }
     }
-
-    val additionalThemes = Option(Play.configuration.getProperty("themes.additionalThemesDir"))
-    additionalThemes match {
-      case Some(s) =>
-        val f = new File(Play.applicationPath + "/" + s).getCanonicalFile
-        if(!f.exists() || !f.isDirectory) {
-          return Error("Incorrectly configured additional themes directory %s".format(s))
-        }
-        val r = new File(f, theme + "/" + relativePath)
-        if(!r.exists()) return NotFound
-        new RenderBinary(r)
-      case None =>
-        NotFound
-    }
+    if (!f.exists()) return NotFound
+    val contentType = MimeTypes.getContentType(f.getName)
+    new RenderBinary(new FileInputStream(f), f.getName, f.length(), contentType, false)
   }
 
 }
