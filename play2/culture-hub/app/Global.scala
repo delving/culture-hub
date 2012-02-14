f581468bb2951b38c9b6dc613382ec30dfd15d3c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-14 15:42:06 +0100

    Akka scheduler for DataSet indexing

--- play2/culture-hub/app/Global.scala
+++ play2/culture-hub/app/Global.scala
@@ -3,16 +3,15 @@
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 
-import com.mongodb.BasicDBObject
-import com.mongodb.casbah.commons.MongoDBObject
-import core.mapping.MappingService
-import java.util.Date
+import jobs._
 import models._
-import org.bson.types.ObjectId
-import org.joda.time.DateTime
+import play.api.libs.concurrent._
+import akka.util.duration._
+import akka.actor._
+import core.mapping.MappingService
 import play.api._
 import play.api.Play.current
-import util.{MissingLibs, ThemeHandler}
+import util.ThemeHandler
 
 object Global extends GlobalSettings {
 
@@ -31,145 +30,33 @@ object Global extends GlobalSettings {
           \____/\__,_/_/\__/\__,_/_/   \___/_/ /_/\__,_/_.___/
     """)
 
-    // load themes
+
+    // ~~~ load themes
     ThemeHandler.startup()
 
-    if (Play.isDev || Play.isTest) {
-      if (User.count() == 0) bootstrapUser()
-      if (Group.count() == 0) bootstrapAccessControl()
-      if (UserCollection.count() == 0) bootstrapUserCollection()
-      if (DObject.count() == 0) bootstrapDObject()
-      if (DataSet.count() == 0) bootstrapDatasets()
-    }
-
+    // ~~~ bootstrap services
     MappingService.init()
 
-  }
 
-  private def bootstrapUser() {
-    val profile = UserProfile()
-    User.insert(new User(
-      _id = new ObjectId("4e5679a80364ae80333ab939"),
-      userName = "bob",
-      firstName = "bob",
-      lastName = "Marley",
-      email = "bob@gmail.com",
-      password = MissingLibs.passwordHash("secret", MissingLibs.HashType.SHA512),
-      userProfile = profile,
-      isActive = true,
-      isHubAdmin = Some(true),
-      nodesAdmin = List("culturehub")
-    ))
-    User.insert(new User(
-      _id = new ObjectId("4e5679a80364ae80333ab93a"),
-      userName = "jimmy",
-      firstName = "Jimmy",
-      lastName = "Hendrix",
-      email = "jimmy@gmail.com",
-      password = MissingLibs.passwordHash("secret", MissingLibs.HashType.SHA512),
-      userProfile = profile,
-      isActive = true
-    ))
-    User.insert(new User(
-      _id = new ObjectId("4e5679a80364ae80333ab93b"),
-      userName = "dan",
-      firstName = "Dan",
-      lastName = "Brown",
-      email = "dan@gmail.com",
-      password = MissingLibs.passwordHash("secret", MissingLibs.HashType.SHA512),
-      userProfile = profile,
-      isActive = true
-    ))
-  }
+    // ~~~ bootstrap jobs
 
-  private def bootstrapAccessControl() {
+    // DataSet indexing
+    val indexer = Akka.system.actorOf(Props[Indexer])
+    Akka.system.scheduler.schedule(
+      0 seconds,
+      10 seconds,
+      indexer,
+      IndexDataSets
+    )
 
-    val delving = Organization(node = "cultureHub", orgId = "delving", name = Map("en" -> "Delving"))
-    val bnf = Organization(node = "cultureHub", orgId = "bnf", name = Map("en" -> "National Library of France", "fr" -> "Bibliotheque nationale de France"))
 
-    val delvingId = Organization.insert(delving)
-    val bnfId = Organization.insert(bnf)
+    // ~~~ load test data
 
-    // all users are in delving
-    User.find(MongoDBObject()).foreach(u => Organization.addUser("delving", u.userName))
-
-    val delvingOwners = Group(node = "cultureHub", name = "Owners", orgId = delving.orgId, grantType = GrantType.OWN.key)
-    val delvingOwnersId = Group.insert(delvingOwners)
-
-    // bob is an owner
-    Group.addUser("bob", delvingOwnersId.get)
+    if (Play.isDev || Play.isTest) {
+      util.TestDataLoader.load()
+    }
 
   }
 
-  private def bootstrapUserCollection() {
-    UserCollection.insert(new UserCollection(
-      TS_update = new DateTime("2011-08-14T10:19:20.835Z").toDate,
-      userName = "bob",
-      name = "Test Collection",
-      description = "A test collection",
-      visibility = Visibility(10),
-      thumbnail_id = None,
-      thumbnail_url = None,
-      deleted = false
-    ))
-  }
 
-  private def bootstrapDObject() {
-    DObject.insert(new DObject(
-      TS_update = new DateTime("2011-08-14T10:19:20.835Z").toDate,
-      userName = "bob",
-      description = "A test object",
-      name = "Test Object A",
-      visibility = Visibility(10),
-      thumbnail_id = None,
-      deleted = false
-    ))
-    DObject.insert(new DObject(
-      TS_update = new DateTime("2011-08-14T10:19:20.835Z").toDate,
-      userName = "jimmy",
-      description = "Another test object",
-      name = "Test Object B",
-      visibility = Visibility(10),
-      thumbnail_id = None,
-      deleted = false
-    ))
-  }
-
-  private def bootstrapDatasets() {
-    var factMap = new BasicDBObject()
-    factMap.put("spec", "PrincesseofSample")
-    factMap.put("name", "Princessehof Sample Dataset")
-    factMap.put("collectionType", "all")
-    factMap.put("namespacePrefix", "raw")
-    factMap.put("language", "nl")
-    factMap.put("country", "netherlands");
-    factMap.put("provider", "Sample Man")
-    factMap.put("dataProvider", "Sample Man")
-    factMap.put("rights", "http://creativecommons.org/publicdomain/mark/1.0/")
-    factMap.put("type", "IMAGE")
-
-    DataSet.insert(DataSet(
-      spec = "PrincessehofSample",
-      user_id = new ObjectId("4e5679a80364ae80333ab939"),
-      orgId = "delving",
-      description = Some("Test Data"),
-      state = DataSetState.ENABLED,
-      visibility = Visibility(10),
-      deleted = false,
-      details = Details(
-        name = "Princessehof Sample Dataset",
-        metadataFormat = RecordDefinition(
-          prefix = "raw",
-          namespace = "http://delving.eu/namespaces/raw",
-          schema = "http://delving.eu/namespaces/raw/schema.xsd",
-          accessKeyRequired = true
-        ),
-        facts = factMap,
-        errorMessage = Some("")
-      ),
-      lastUploaded = new Date(0),
-      idxMappings = List("icn"),
-      mappings = Map("icn" -> Mapping(format = RecordDefinition.recordDefinitions.filter(rDef => rDef.prefix == "icn").head))
-    ))
-  }
 }
