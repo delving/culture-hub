3dc286931ba0b11b35c54da634dfa32bb19e984a
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-15 17:29:39 +0100

    DeepZoom demo

---
+++ play2/culture-hub/app/controllers/Demo.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import play.api.mvc._
+import play.api.Play
+import play.api.Play.current
+import play.api.mvc.Results._
+
+/**
+ * Various things, for demonstration purposes
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+
+object Demo extends DelvingController {
+
+  /**
+   * DeepZoom viewer
+   */
+  def view(image: Option[String]) = Root {
+    Action {
+      implicit request =>
+        val smallballs = current.getFile("/public/images/smallballs.tif").getAbsolutePath
+        if (image.isEmpty)
+          Ok(Template('image -> smallballs))
+        else
+          Ok(Template('image -> (image)))
+    }
+  }
+
+  def iipsrv = Root {
+    Action {
+      implicit request =>
+        Redirect(Play.configuration.getString("iipsrv.url").getOrElse("") + "?" + request.rawQueryString, MOVED_PERMANENTLY)
+    }
+  }
+
+}
