5201871079e575695afaa5478b7a844fc0d1a1d5
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-21 15:09:11 +0100

    Cleaning up unused module build

--- play2/culture-hub/project/Build.scala
+++ play2/culture-hub/project/Build.scala
@@ -18,19 +18,20 @@ object ApplicationBuild extends Build {
     "net.liftweb"               %% "lift-json-ext"                   % "2.4-M4",
 
     "eu.delving"                %  "sip-core"                        % "0.4.6-SNAPSHOT",
+    "eu.delving"                %  "sip-creator"                     % "0.4.6-SNAPSHOT",
 
     "org.apache.solr"           %  "solr-solrj"                      % "3.4.0",
     "org.apache.tika"           %  "tika-parsers"                    % "1.0"
 
   )
 
+  val dosDependencies = Seq(
+    "com.thebuzzmedia"          %  "imgscalr-lib"                     % "3.2"
+  )
+
   val frameworkExtensions = Project("framework-extensions", file("modules/framework-extensions")).settings(
     resolvers += delvingSnapshots,
     resolvers += delvingReleases
-  )
-
-  val dosDependencies = Seq(
-    "com.thebuzzmedia"         %  "imgscalr-lib"                     % "3.2"
   )
 
   val dos = PlayProject("dos", dosVersion, dosDependencies, path = file("modules/dos")).dependsOn(frameworkExtensions).settings(
