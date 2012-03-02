35236094f435dd24f993c59d1fc3e791717617c4
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-03-02 19:47:49 +0100

    In-memory implementation of all services, for development

--- app/core/UserProfileService.scala
+++ app/core/UserProfileService.scala
@@ -14,14 +14,14 @@ trait UserProfileService {
 
 }
 
-case class UserProfile(isPublic:      Boolean,
+case class UserProfile(isPublic:      Boolean = false,
                        firstName:     String,
                        lastName:      String,
                        email:         String,
-                       description:   Option[String],
-                       funFact:       Option[String],
-                       websites:      List[String],
-                       twitter:       Option[String],
-                       linkedIn:      Option[String]
+                       description:   Option[String] = None,
+                       funFact:       Option[String] = None,
+                       websites:      List[String] = List.empty,
+                       twitter:       Option[String] = None,
+                       linkedIn:      Option[String] = None
                       )
 
