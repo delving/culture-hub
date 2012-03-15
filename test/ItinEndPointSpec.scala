11a8b8023cf51c4f3e509ba7acf65a40aed530b5
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2012-03-15 14:47:35 +0100

    next iteration of the ItinEndpoint

---
+++ test/ItinEndPointSpec.scala
@@ -0,0 +1,34 @@
+import org.specs2.mutable.Specification
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ *
+ * @author Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
+ * @since 3/15/12 8:40 AM  
+ */
+
+class ItinEndPointSpec extends Specification with Cleanup {
+//
+//  "ItinEndPoint" should {
+//
+//    "Store XML post" in {
+
+//      running(FakeApplication()) {
+//        val result = controllers.custom.ItinEndPoint.store()(FakeRequest())
+//        status(result) must equalTo(OK)
+//        contentAsString(result) must contain("<spec>PrincessehofSample</spec>")
+//      }
+//
+//    }
+//  }
+
+  step(cleanup)
+
+
+  val itinTestXml =
+  """
+
+  """
+
+}
