d026c85b77d0ebe750f57963adb5b527e0a165c6
zemyatin <eric@delving.eu>
2012-03-29 10:18:06 +0200

    reimplementation of result layout with mouse-over effects

--- public/themes/default/javascripts/index.js
+++ public/themes/default/javascripts/index.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
 //    $('.carousel').carousel();
-//    $(".ic_container").capslide({
-//        caption_color    : 'white',
-//        caption_bgcolor    : 'black',
-//        overlay_bgcolor : 'black',
-//        border            : '',
-//        showcaption        : true
-//    });
+    $(".ic_container").capslide({
+        caption_color    : 'white',
+        caption_bgcolor    : 'black',
+        overlay_bgcolor : 'black',
+        border            : '',
+        showcaption        : true
+    });
 });
