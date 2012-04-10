ae147be4fb63f103cd6283b7d57e0aa9785832e9
zemyatin <eric@delving.eu>
2012-04-10 22:41:07 +0200

    addition of 'hero-unit' template

--- public/common/javascripts/tiny-templates.js
+++ public/common/javascripts/tiny-templates.js
@@ -3,5 +3,6 @@ var tinyMCETemplateList = [
     ["2 column grid", "/assets/common/templates/mce/1row2col.template", "A single row with two columns"],
     ["3 column grid", "/assets/common/templates/mce/1row3col.template", "A single row with three columns"],
     ["Table", "/assets/common/templates/mce/table.template", "A nice basic table"],
-    ["Table striped", "/assets/common/templates/mce/table-striped.template", "Table with alternating row colors"]
+    ["Table striped", "/assets/common/templates/mce/table-striped.template", "Table with alternating row colors"],
+    ["Hero unit", "/assets/common/templates/mce/hero-unit.template", "A space to call extra attention to content or information"]
 ];
