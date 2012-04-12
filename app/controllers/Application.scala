43281f485c89dc0221e6b8b82fa76543ff5b2f0b
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2012-04-12 08:49:50 +0200

    Fixed issue with error being thrown when no cms page is created for the homepage

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,15 +14,17 @@ object Application extends DelvingController {
       implicit request =>
         val themeInfo = renderArgs("themeInfo").get.asInstanceOf[ThemeInfo]
         val recentCollections: List[ListItem] = UserCollection.findRecent(themeInfo.themeProperty("recentCollectionsCount", classOf[Int])).toList
-        val recentStories: List[ListItem] = Story.findRecent(themeInfo.themeProperty("recentStoriesCount", classOf[Int])).toList
+        val recentStories: List[Story] = Story.findRecent(themeInfo.themeProperty("recentStoriesCount", classOf[Int])).toList
         val recentObjects: List[ListItem] = DObject.findRecent(themeInfo.themeProperty("recentObjectsCount", classOf[Int])).toList
         val recentMdrs: List[ListItem] = Search.search(None, 1, theme, List("%s:%s AND %s:%s".format(RECORD_TYPE, MDR, HAS_DIGITAL_OBJECT, true)))._1.slice(0, themeInfo.themeProperty("recentMdrsCount", classOf[Int]))
-        val homepageCmsContent =  CMSPage.find(MongoDBObject("key" -> "homepage", "lang" -> getLang, "theme" -> theme.name)).$orderby(MongoDBObject("_id" -> -1)).limit(1).toList.headOption match {
-          case None => NotFound("homepage")
-          case Some(homepageCmsContent) => homepageCmsContent
-        }
+        val homepageCmsContent =  CMSPage.find(MongoDBObject("key" -> "homepage", "lang" -> getLang, "theme" -> theme.name)).$orderby(MongoDBObject("_id" -> -1)).limit(1).toList.headOption
 
-        Ok(Template('recentCollections -> recentCollections, 'recentStories -> recentStories, 'recentObjects -> recentObjects, 'recentMdrs -> recentMdrs, 'homepageCmsContent -> homepageCmsContent))
+        homepageCmsContent match {
+        case None =>
+          Ok(Template('recentCollections -> recentCollections, 'recentStories -> recentStories, 'recentObjects -> recentObjects, 'recentMdrs -> recentMdrs))
+        case Some(cmsContent) =>
+          Ok(Template('recentCollections -> recentCollections, 'recentStories -> recentStories, 'recentObjects -> recentObjects, 'recentMdrs -> recentMdrs, 'homepageCmsContent -> cmsContent))
+        }
     }
   }
 
