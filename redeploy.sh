551350b3d6fa349dced6904629e685c2e4e3786f
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-04-19 10:40:38 +0200

    Also downloading sip-creator

--- redeploy.sh
+++ redeploy.sh
@@ -10,5 +10,6 @@ echo
 export _JAVA_OPTIONS="-Dconfig.file=`pwd`/conf/production.conf -Xms256M -Xmx1024M"
 kill -9 `cat RUNNING_PID` && rm RUNNING_PID
 git pull
+ant downloadSipCreator
 ../play-2.0/play clean
 ../play-2.0/play start &
