1d2b0bc46702db5aa660b64fd59636d721caa06c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-04-24 15:48:40 +0200

    Reviving SOLR search proxy

--- app/controllers/organization/Admin.scala
+++ app/controllers/organization/Admin.scala
@@ -7,6 +7,10 @@ import play.api.mvc.Action
 import util.ThemeHandler
 import models._
 import core.HubServices
+import core.search.Params
+import play.api.Play
+import play.api.Play.current
+import play.api.libs.ws.WS
 
 /**
  *
@@ -74,6 +78,24 @@ object Admin extends OrganizationController {
         val reIndexable = DataSet.findByState(DataSetState.ENABLED).toList
         reIndexable foreach { r => DataSet.updateStateAndProcessingCount(r, DataSetState.QUEUED)}
         Ok("Queued %s DataSets for indexing".format(reIndexable.size))
+    }
+  }
+
+  def solrSearchProxy(orgId: String) = OrgOwnerAction(orgId) {
+    Action {
+      implicit request =>
+        val solrQueryString: String = request.rawQueryString
+        val solrServerUrl: String = String.format("%s/select?%s", Play.configuration.getString("solr.baseUrl").getOrElse("http://localhost:8983/solr/core2"), solrQueryString)
+
+        val response = WS.url(solrServerUrl).get().await.fold(
+          error => Left(error),
+          success => Right(success)
+        )
+
+        response match {
+          case Right(r) => Ok(r.xml)
+          case Left(error) => InternalServerError(error.getMessage)
+        }
     }
   }
 
