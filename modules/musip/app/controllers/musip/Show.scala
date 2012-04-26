749b70f1a23c24de5cc8d0d744d9492ed60cd4c6
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-04-26 21:24:41 +0200

    Generifying display

--- modules/musip/app/controllers/musip/Show.scala
+++ modules/musip/app/controllers/musip/Show.scala
@@ -1,10 +1,10 @@
 package controllers.musip
 
-import play.api.mvc.Action
 import controllers.DelvingController
 import core.rendering.ViewRenderer
 import models.MusipItem
 import play.api.i18n.Lang
+import play.api.mvc.{RequestHeader, Action}
 
 
 /**
@@ -25,22 +25,24 @@ object Show extends DelvingController {
 
   def museum(orgId: String, itemId: String) = Root {
     Action {
-      implicit request => {
-
-        MusipItem.find(orgId, itemId, "museum") match {
-          case Some(museum) =>
-            if(!museumViewDefinition.isDefined) {
-              InternalServerError("Could not find museum view definition")
-            } else {
-              val renderResult = museumViewDefinition.get.renderRecord(museum.rawXml, List.empty, Map("musip" -> "http://www.musip.nl/"), Lang(getLang))
-              val viewTree = renderResult.toViewTree
-
-              Ok(Template("museum.html", 'view -> viewTree))
-            }
-
-          case None => NotFound("Museum not found")
-        }
+      implicit request => show(orgId, itemId, "museum", museumViewDefinition) match {
+        case Some((viewTree, systemFields)) => Ok(Template("show.html", 'view -> viewTree, 'systemFields -> systemFields))
+        case None => NotFound("Museum not found")
       }
+    }
+  }
+
+  private def show(orgId: String, itemId: String, itemType: String, renderer: Option[ViewRenderer])(implicit request: RequestHeader) = {
+    MusipItem.find(orgId, itemId, itemType).map {
+      thing =>
+        if (!renderer.isDefined) {
+          InternalServerError("Could not find renderer for " + itemType)
+        } else {
+          val renderResult = renderer.get.renderRecord(thing.rawXml, List.empty, Map("musip" -> "http://www.musip.nl/"), Lang(getLang))
+          val viewTree = renderResult.toViewTree
+
+          (viewTree, thing.systemFields)
+        }
     }
   }
 
