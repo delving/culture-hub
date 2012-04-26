e09b906f1c2d0b70056d49c326a94fc88c32b26c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-04-26 21:02:22 +0200

    Generifying musip item synchronization

--- modules/musip/app/controllers/musip/Admin.scala
+++ modules/musip/app/controllers/musip/Admin.scala
@@ -5,10 +5,10 @@ import play.api.mvc.Action
 import play.api.Play
 import play.api.Play.current
 import models.MusipItem
-import com.mongodb.casbah.Imports._
 import org.apache.solr.common.SolrInputDocument
 import core.Constants._
 import core.indexing.IndexingService
+import scala.xml._
 
 /**
  *
@@ -17,50 +17,66 @@ import core.indexing.IndexingService
 
 object Admin extends OrganizationController {
 
+  type MultiMap = Map[String, List[String]]
+
   def synchronize(orgId: String) = OrgOwnerAction(orgId) {
     Action {
       implicit request =>
-        val r = Play.application.resource("/musip_actors.xml")
+        val syncedMuseums = Play.application.resource("/musip_actors.xml").map(r => scala.xml.XML.load(r) \ "actor").map(sync(_, orgId, "museum", {
+          museum =>
+            Map(
+                TITLE -> List((museum \ "name").text.trim),
+                DESCRIPTION -> List((museum \ "description").text.trim),
+                THUMBNAIL -> List((museum \ "image").text.trim)
+               )
+        })).getOrElse(0)
 
-        if (r.isDefined) {
-          val url = r.get
-          val actors = scala.xml.XML.load(url) \ "actor"
+        val syncedCollections = Play.application.resource("/musip_collections.xml").map(r => scala.xml.XML.load(r) \ "collection").map(sync(_, orgId, "collection", {
+          collection =>
+            Map(
+                TITLE -> List((collection \ "name").text.trim),
+                DESCRIPTION -> List((collection \ "description").text.trim),
+                THUMBNAIL -> List((collection \ "image").text.trim)
+               )
+        })).getOrElse(0)
 
-          IndexingService.deleteByQuery("delving_orgId:%s delving_recordType:museum delving_systemType:hubItem".format(orgId))
-
-          for (actor <- actors) {
-            val localId = (actor \ "@localId").text
-
-            val item = MusipItem(rawXml = actor.toString(), orgId = orgId, itemId = localId, itemType = "museum")
-            MusipItem.saveOrUpdate(item, orgId, "museum")
-
-            // indexing
-            val name = (actor \ "name").text.trim
-            val description = (actor \ "description").text.trim
-            val thumbnail = (actor \ "image").text.trim
-
-            val doc = new SolrInputDocument()
-
-            doc.addField(ID, "%s_%s_%s".format(orgId, "musip", localId))
-            doc.addField(ORG_ID, orgId)
-            doc.addField(RECORD_TYPE, "museum")
-            doc.addField(VISIBILITY, "10")
-            doc.addField(SYSTEM_TYPE, HUB_ITEM)
-            doc.addField(TITLE, name)
-            doc.addField(DESCRIPTION, description)
-            doc.addField(THUMBNAIL, thumbnail)
-            doc.addField(HUB_URI, "/%s/museum/%s".format(orgId, localId))
-
-            IndexingService.stageForIndexing(doc)
-          }
-
-          IndexingService.commit()
-
-          Ok("Synchronized %s items".format(actors.size))
-        } else {
-          Ok("Found no data to synchronize")
-        }
+        Ok("Synchronized %s museums and %s collections".format(syncedMuseums, syncedCollections))
     }
   }
+
+  private def sync(items: NodeSeq, orgId: String, itemType: String, extractSystemFields: NodeSeq => MultiMap): Int = {
+    IndexingService.deleteByQuery("delving_orgId:%s delving_recordType:%s delving_systemType:hubItem".format(orgId, itemType))
+    for (i <- items) {
+      val localId = (i \ "@localId").text
+      val xml = i.toString()
+
+      // extract system fields
+      val systemFields = Map(
+        TITLE -> List((i \ "name").text.trim),
+        DESCRIPTION -> List((i \ "description").text.trim),
+        THUMBNAIL -> List((i \ "image").text.trim)
+      )
+
+      val item = MusipItem(rawXml = xml, orgId = orgId, itemId = localId, itemType = itemType, systemFields = systemFields)
+      MusipItem.saveOrUpdate(item, orgId, itemType)
+
+      val doc = new SolrInputDocument()
+
+      systemFields.foreach {
+        sf => doc.addField(sf._1, sf._2)
+      }
+
+      doc.addField(ID, "%s_%s_%s".format(orgId, itemType, localId))
+      doc.addField(ORG_ID, orgId)
+      doc.addField(RECORD_TYPE, itemType)
+      doc.addField(SYSTEM_TYPE, HUB_ITEM)
+      doc.addField(HUB_URI, "/%s/%s/%s".format(orgId, itemType, localId))
+
+      IndexingService.stageForIndexing(doc)
+    }
+    IndexingService.commit()
+    items.size
+  }
+
 
 }
