1c51fb0eacb41b15a668b1b2625034b06676ba02
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-02 16:12:25 +0200

    Fixing startup scripts for good. Production logger, without console output

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-LOGFILE=logs/application.log
-
 echo
 echo
 echo =============================================
@@ -9,19 +7,24 @@ echo Redeploying the CultureHub in production mode
 echo =============================================
 echo
 echo
-export _JAVA_OPTIONS="-Dconfig.file=`pwd`/conf/production.conf -Xms256M -Xmx1024M"
+export _JAVA_OPTIONS="-Dconfig.file=`pwd`/conf/production.conf -Dlogger.resource=conf/prod-logger.xml -Xms256M -Xmx1024M"
 kill -9 `cat RUNNING_PID` && rm RUNNING_PID
 git pull
 ant downloadSipCreator
 ../play-2.0/play clean
 ../play-2.0/play compile
 ../play-2.0/play start &
-SBT_PID=$!
-echo SBT running on PID $SBT_PID
-echo Don't forget to kill it when it is done
 
-#TODO FIXME
-#while ! (tail $LOGFILE | grep -qi Listening); do
-#  sleep 1
-#done
-#kill -9 $SBT_PID
+echo
+echo "Starting Play. Please wait..."
+echo
+
+sleep 30
+echo
+echo Killing SBT
+echo
+ps -ef | grep "/play-2.0/framework/sbt/sbt-launch.jar start" | grep -v grep | awk '{print $2}' | xargs kill -9
+
+echo
+echo "Script done, but server likely not up yet. Check logs/application.log to see startup progress"
+echo
--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-LOGFILE=logs/application.log
-
 echo
 echo
 echo ==========================================
@@ -9,13 +7,19 @@ echo Starting the CultureHub in production mode
 echo ==========================================
 echo
 echo
-export _JAVA_OPTIONS="-Dconfig.file=`pwd`/conf/production.conf -Xms256M -Xmx1024M"
+export _JAVA_OPTIONS="-Dconfig.file=`pwd`/conf/production.conf -Dlogger.resource=prod-logger.xml -Xms256M -Xmx1024M"
 ../play-2.0/play start &
-SBT_PID=$!
-echo SBT running on PID $SBT_PID
-echo Don't forget to kill it when it is done
 
-#while ! (tail $LOGFILE | grep -qi Listening); do
-#  sleep 1
-#done
-#kill -9 $SBT_PID
+echo
+echo "Starting Play. Please wait..."
+echo
+
+sleep 30
+echo
+echo Killing SBT
+echo
+ps -ef | grep "/play-2.0/framework/sbt/sbt-launch.jar start" | grep -v grep | awk '{print $2}' | xargs kill -9
+
+echo
+echo "Script done, but server likely not up yet. Check logs/application.log to see startup progress"
+echo
