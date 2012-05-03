48a192d91f0dd9a94b9bee6eea64ed7559c84119
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-03 13:02:38 +0200

    Replacing IndexItem by MetadataItem

--- app/controllers/api/Index.scala
+++ app/controllers/api/Index.scala
@@ -3,14 +3,14 @@ package controllers.api
 import controllers.DelvingController
 import play.api.mvc._
 import play.api.libs.concurrent.Promise
-import models.IndexItem
 import scala.xml._
 import collection.mutable.ListBuffer
 import core.Constants._
 import core.indexing.IndexingService
-import com.mongodb.casbah.commons.MongoDBObject
+import models.{MetadataItem, MetadataCache}
+import org.bson.types.ObjectId
+import org.apache.solr.common.SolrInputDocument
 import org.joda.time.format.ISODateTimeFormat
-import play.api.Logger
 
 /**
  *
@@ -18,6 +18,8 @@ import play.api.Logger
  */
 
 object Index extends DelvingController {
+
+  val CACHE_COLLECTION = "indexApiItems"
 
   def explain(path: List[String]) = path match {
     case Nil =>
@@ -83,17 +85,20 @@ object Index extends DelvingController {
           var indexed: Int = 0
           var deleted: Int = 0
 
-          valid.foreach {
-            item =>
-              if(item.deleted) {
-                IndexItem.remove(item.orgId, item.itemId, item.itemType)
-                IndexingService.deleteByQuery("""id:%s_%s_%s""".format(item.orgId, item.itemType, item.itemId))
-                deleted += 1
-              } else {
-                IndexItem.update(MongoDBObject("itemId" -> item.itemId, "orgId" -> orgId, "itemType" -> item.itemType), IndexItem._grater.asDBObject(item), true)
-                IndexingService.stageForIndexing(item.toSolrDocument)
-                indexed += 1
-              }
+          for(i <- valid.zipWithIndex) {
+            val item = i._1
+            val index = i._2
+            val cache = MetadataCache.get(orgId, CACHE_COLLECTION, item.itemType)
+            if(item.deleted) {
+              cache.remove(item.itemId)
+              IndexingService.deleteByQuery("""id:%s_%s_%s""".format(item.orgId, item.itemType, item.itemId))
+              deleted += 1
+            } else {
+              val cacheItem = MetadataItem(collection = CACHE_COLLECTION, itemType = item.itemType, itemId = item.itemId, xml = Map("raw" -> item.rawXml), index = index)
+              cache.saveOrUpdate(cacheItem)
+              IndexingService.stageForIndexing(item.toSolrDocument)
+              indexed += 1
+            }
           }
           IndexingService.commit()
 
@@ -168,6 +173,75 @@ object Index extends DelvingController {
     (validItems.toList, invalidItems.toList)
 
   }
+}
 
+case class IndexItem(_id: ObjectId = new ObjectId,
+                     orgId: String,
+                     itemId: String,
+                     itemType: String,
+                     rawXml: String,
+                     deleted: Boolean = false) {
+
+  def toSolrDocument: SolrInputDocument = {
+    val doc = new SolrInputDocument
+
+    val document = XML.loadString(rawXml).nonEmptyChildren
+    val fields = document.filter(_.label == "field")
+
+    // content fields
+    fields.filter(_.attribute("name").isDefined).foreach {
+      field =>
+        val name = (field \ "@name").text
+        val dataType = field.attribute("fieldType").getOrElse("text")
+        val isFacet = field.attribute("facet").isDefined && (field \ "@facet").text == "true"
+
+        val acceptedPrefixes = "dc|dcterms|icn|europeana|delving"
+
+        val indexFieldName = if (name.contains(":") && name.split(":").head.matches(acceptedPrefixes)) {
+          "%s_%s".format(name.replaceFirst(":", "_"), dataType)
+        }
+        else if (name.contains("_") && name.split("_").head.matches(acceptedPrefixes)) {
+          "%s_%s".format(name, dataType)
+        }
+        else {
+          "custom_%s_%s".format(name, dataType)
+        }
+
+        doc.addField(indexFieldName, field.text)
+
+        if(isFacet) {
+          doc.addField(indexFieldName + "_facet", field.text)
+        }
+    }
+
+    // system fields
+    val allowedSystemFields = List("collection", "thumbnail", "landingPage", "provider", "owner", "title", "description", "fullText")
+
+    val systemFields = document.filter(_.label == "systemField")
+    systemFields.filter(f => f.attribute("name").isDefined && allowedSystemFields.contains(f.attribute("name").get.text)).foreach {
+      field =>
+        val name = (field \ "@name").text
+
+        name match {
+          case "thumbnail" => doc.addField(THUMBNAIL, field.text)
+          case "title" | "description" | "fullText" =>
+            doc.addField("delving_%s_%s".format(name, "text"), field.text)
+          case _ =>
+            val indexFieldName = "delving_%s_%s".format(name, "string")
+            doc.addField(indexFieldName, field.text)
+        }
+    }
+
+    // mandatory fields
+    val id = "%s_%s_%s".format(orgId, itemType, itemId)
+    doc.addField(ID, id)
+    doc.addField(HUB_ID, id)
+    doc.addField(ORG_ID, orgId)
+
+    doc.addField(SYSTEM_TYPE, INDEX_API_ITEM)
+    doc.addField(RECORD_TYPE, itemType)
+
+    doc
+  }
 
 }
