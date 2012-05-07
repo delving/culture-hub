cd38d9a89dedaa2777b49141f9b7c2410abea01a
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-07 10:11:28 +0200

    More BaseX commands

--- modules/basex-scala-client/src/test/scala/eu/delving/basex/client/BaseXSpec.scala
+++ modules/basex-scala-client/src/test/scala/eu/delving/basex/client/BaseXSpec.scala
@@ -10,22 +10,38 @@ import org.specs2.mutable.Specification
 
 class BaseXSpec extends Specification {
 
-  var s: BaseX = null
-
-  step {
-    s = new BaseX("localhost", 1984, "admin", "admin")
+  var s: BaseX = {
+    val server = new BaseX("localhost", 1984, "admin", "admin")
+    server.start()
+    server
   }
 
   "the BaseX storage" should {
 
-    "insert a document" in {
-
+    "create a database" in {
       s.createDatabase("test")
-      s.add("test", "foo.xml", "<root><bla>bar</bla></root>")
+    }
 
+    "insert a document" in {
+      s.add("test", "foo.xml", "<root><bla>bar</bla></root>")
       val r = s.query("test", "//root")
+      r.size must be equalTo (1)
+    }
+
+    "replace a document" in {
+      s.replace("test", "foo.xml", "<replacedRoot><bla>bar</bla></replacedRoot>")
+
+      val r = s.query("test", "//replacedRoot")
+      val r1 = s.query("test", "//root")
 
       r.size must be equalTo (1)
+      r1.size must be equalTo (0)
+    }
+
+    "delete a document" in {
+      s.delete("test", "foo.xml")
+      val r = s.query("test", "//replacedRoot")
+      r.size must be equalTo (0)
     }
 
     "fail to insert a document in a non-existing db" in {
@@ -34,6 +50,9 @@ class BaseXSpec extends Specification {
 
   }
 
-  step(s.shutdown())
+  step {
+    s.dropDatabase("test")
+    s.stop()
+  }
 
 }
