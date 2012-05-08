b6158665cd9df3df24747bb77b4e5134731ae96c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-08 17:46:46 +0200

    More convenience methods

--- modules/basex-scala-client/src/main/scala/eu/delving/basex/client/package.scala
+++ modules/basex-scala-client/src/main/scala/eu/delving/basex/client/package.scala
@@ -1,6 +1,9 @@
 package eu.delving.basex.client
 
-import org.basex.server.ClientQuery
+import org.basex.server.{ClientSession, ClientQuery}
+import org.basex.core.cmd.Flush
+import xml.{XML, Node}
+
 
 object `package` extends Implicits
 
@@ -11,5 +14,34 @@ trait Implicits {
     def next(): String = query.next()
   }
 
+  class RichClientSession(session: ClientSession) {
+
+    def open(db: String) {
+      session.execute("open " + db)
+    }
+
+    def find(query: String): Iterator[Node] = {
+      session.query(query).map(XML.loadString(_))
+    }
+
+    def setAutoflush(flush: Boolean) {
+      if(flush) {
+        session.execute("set autoflush true")
+      } else {
+        session.execute("set autoflush false")
+      }
+    }
+
+    def flush() {
+      session.execute(new Flush())
+    }
+
+    def createAttributeIndex() {
+      session.execute("create index attribute")
+    }
+
+  }
+
   implicit def withRichClientQuery[A <: ClientQuery](query: A): RichClientQuery = new RichClientQuery(query)
+  implicit def withRichClientSession[A <: ClientSession](session: A): RichClientSession = new RichClientSession(session)
 }
