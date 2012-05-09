b7437be4af93eaac6007418a15dd7007752c5ce1
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2012-05-09 11:54:10 +0200

    Now the IndexApi also accepts some of our default namespaces (cherry picked from commit 69824d9)

--- core/app/models/IndexItem.scala
+++ core/app/models/IndexItem.scala
@@ -33,9 +33,19 @@ case class IndexItem(_id: ObjectId = new ObjectId,
         val dataType = field.attribute("fieldType").getOrElse("text")
         val isFacet = field.attribute("facet").isDefined && (field \ "@facet").text == "true"
 
-        val indexFieldName = "%s_%s".format(name, dataType)
+        val acceptedPrefixes = "dc|dcterms|icn|europeana|delving"
 
-        doc.addField("custom_%s".format(indexFieldName), field.text)
+        val indexFieldName = if (name.contains(":") && name.split(":").head.matches(acceptedPrefixes)) {
+          "%s_%s".format(name.replaceFirst(":", "_"), dataType)
+        }
+        else if (name.contains("_") && name.split("_").head.matches(acceptedPrefixes)) {
+          "%s_%s".format(name, dataType)
+        }
+        else {
+          "custom_%s_%s".format(name, dataType)
+        }
+
+        doc.addField(indexFieldName, field.text)
 
         if(isFacet) {
           doc.addField(indexFieldName + "_facet", field.text)
