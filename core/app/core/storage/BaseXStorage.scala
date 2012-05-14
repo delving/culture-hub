bf9e7c1feb71d23de227f1ce88f094c946636080
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-14 16:53:08 +0200

    Making sure the database exists before counting its records

--- core/app/core/storage/BaseXStorage.scala
+++ core/app/core/storage/BaseXStorage.scala
@@ -5,7 +5,7 @@ import eu.delving.basex.client.BaseX
 import org.basex.server.ClientSession
 import java.io.ByteArrayInputStream
 import scala._
-import xml.{XML, Node}
+import xml.Node
 
 /**
  * BaseX-based Storage engine.
@@ -25,6 +25,8 @@ object BaseXStorage {
     storage.createDatabase(c.databaseName)
     c
   }
+
+  def openCollection(c: Collection): Option[Collection] = openCollection(c.orgId, c.name)
 
   def openCollection(orgId: String, collectionName: String): Option[Collection] = {
     val c = Collection(orgId, collectionName)
@@ -75,8 +77,14 @@ object BaseXStorage {
   }
 
   def count(collection: Collection): Int = {
-    withSession(collection) {
-      implicit session => count
+    val c = openCollection(collection)
+    c.map {
+      col =>
+        withSession(col) {
+          implicit session => count
+        }
+    }.getOrElse {
+      0
     }
   }
 
