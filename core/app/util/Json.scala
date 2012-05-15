9d3d7709c80ef276b76f96950d4e96eda5a2f502
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-15 10:19:00 +0200

    Filtering out namespace prefixes for JSON rendering

--- core/app/util/Json.scala
+++ core/app/util/Json.scala
@@ -14,10 +14,11 @@ object Json {
    * Turns a scala XML node into a JSON string
    *
    * @param xml the input xml document
+   * @param removeNamespaces removes XML namespace declarations
    * @param sequences field names that are sequences, and should be generated as array even when there's only a single element
    * @return a string formatted as JSON
    */
-  def toJson(xml: NodeSeq, sequences: Seq[String] = List.empty): String = {
+  def toJson(xml: NodeSeq, removeNamespaces: Boolean = false, sequences: Seq[String] = List.empty): String = {
     val json = Xml.toJson(xml) map {
         case JField(name: String, x: JObject) =>
           if(sequences.contains(name))
@@ -26,7 +27,13 @@ object Json {
             x
         case x => x
     }
-    Printer.pretty(render(json))
+    val js = if(removeNamespaces) {
+      json.transform {
+        case JField(name, v) if(name.contains(":")) => JField(name.split(":").tail.mkString, v)
+      }
+    } else json
+
+    Printer.pretty(render(js))
   }
 
 
