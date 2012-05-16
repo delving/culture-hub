bc69a4cb1120c84da0f453c95a443efb16d67713
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2012-05-16 16:14:19 +0200

    Updated the solr streaming update configuration

--- core/app/core/indexing/IndexingService.scala
+++ core/app/core/indexing/IndexingService.scala
@@ -91,10 +91,15 @@ object IndexingService extends SolrServer {
     val deleteQuery = SPEC + ":" + spec + " AND " + ORG_ID + ":" + orgId + " AND timestamp:[* TO " + fmt.print(startIndexing.minusSeconds(15)) + "]"
     val orphans = getSolrServer.query(new SolrQuery(deleteQuery)).getResults.getNumFound
     if (orphans > 0) {
-      val deleteResponse = getStreamingUpdateServer.deleteByQuery(deleteQuery)
-      deleteResponse.getStatus
-      getStreamingUpdateServer.commit
-      Logger.info("Deleting orphans %s from dataset from Solr Index: %s".format(orphans.toString, deleteQuery))
+      try {
+        val deleteResponse = getStreamingUpdateServer.deleteByQuery(deleteQuery)
+        deleteResponse.getStatus
+        getStreamingUpdateServer.commit
+        Logger.info("Deleting orphans %s from dataset from Solr Index: %s".format(orphans.toString, deleteQuery))
+      }
+      catch {
+        case e: Exception => Logger.info("Unable to remove orphans for %s because of %s".format(spec, e.getMessage))
+      }
     }
     else
       Logger.info("No orphans found for dataset in Solr Index: %s".format(deleteQuery))
