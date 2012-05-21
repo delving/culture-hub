f340e112b20403f2dc53f1c1db6a48ee2608989b
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-05-21 22:04:52 +0200

    Removing strange control characters from JNLP

--- app/controllers/organization/SipCreator.scala
+++ app/controllers/organization/SipCreator.scala
@@ -27,57 +27,59 @@ object SipCreator extends OrganizationController {
 
         val jnlp = <jnlp spec="1.0+" codebase={codebase} href={home + "sip-creator.jnlp"}>
           <information>
-              <title>SIP-Creator</title>
-              <vendor>Delving</vendor>
-              <description kind="one-line">SIP-Creator</description>
-              <description kind="short">Submission Information Package Creator</description>
+            <title>SIP-Creator</title>
+            <vendor>Delving</vendor>
+            <description kind="one-line">SIP-Creator</description>
+            <description kind="short">Submission Information Package Creator</description>
               <icon href={codebase + "sip-creator-logo.png"} kind="default"/>
-              <shortcut online="false">
-                  <desktop/>
-              </shortcut>
+            <shortcut online="false">
+                <desktop/>
+            </shortcut>
           </information>
           <security>
               <all-permissions/>
           </security>
-          <resources>
-             <j2se version="1.6+" initial-heap-size="256m" max-heap-size="512m"/>
-             <property name="jnlp.versionEnabled" value="false"/>
-             <jar href={"sip-app-" + BuildInfo.sipCreator + ".jar"} main="true"/>
-             <jar href={"sip-core-" + BuildInfo.sipCreator + ".jar"}/>
-             <jar href="oauth2-client-0.2-SNAPSHOT.jar"/>
-             <jar href="oauth2-common-0.2-SNAPSHOT.jar"/>
-             <jar href="jettison-1.2.jar"/>
-             <jar href="slf4j-api-1.6.1.jar"/>
-             <jar href="httpclient-4.1.2.jar"/>
-             <jar href="httpcore-4.1.2.jar"/>
-             <jar href="commons-logging-1.1.1.jar"/>
-             <jar href="commons-codec-1.4.jar"/>
-             <jar href="log4j-1.2.16.jar"/>
-             <jar href="commons-lang-2.3.jar"/>
-             <jar href="commons-io-2.0.jar"/>
-             <jar href="xstream-1.4.2.jar"/>
-             <jar href="xmlpull-1.1.3.1.jar"/>
-             <jar href="xpp3_min-1.1.4c.jar"/>
-             <jar href="gson-1.7.1.jar"/>
-             <jar href="groovy-all-1.8.5.jar"/>
-             <jar href="woodstox-core-asl-4.0.9.jar"/>
-             <jar href="stax-api-1.0-2.jar"/>
-             <jar href="stax2-api-3.0.3.jar"/>
-             <jar href="jfreechart-1.0.13.jar"/>
-             <jar href="jcommon-1.0.16.jar"/>
-             <jar href="itext-2.1.7.jar"/>
-             <jar href="bcmail-jdk14-138.jar"/>
-             <jar href="bcprov-jdk14-138.jar"/>
-             <jar href="bctsp-jdk14-1.38.jar"/>
-             <jar href="bcprov-jdk14-1.38.jar"/>
-             <jar href="bcmail-jdk14-1.38.jar"/>
-             <jar href="stringtemplate-3.0.jar"/>
-             <jar href="antlr-2.7.7.jar"/>
-             <jar href="cglib-2.1_3.jar"/>
-             <jar href="asm-1.5.3.jar"/>
-          </resources>
+          <resources>
+              <j2se version="1.6+" initial-heap-size="256m" max-heap-size="512m"/>
+              <property name="jnlp.versionEnabled" value="false"/>
+              <jar href={"sip-app-" + BuildInfo.sipCreator + ".jar"} main="true"/>
+              <jar href={"sip-core-" + BuildInfo.sipCreator + ".jar"}/>
+              <jar href="oauth2-client-0.2-SNAPSHOT.jar"/>
+              <jar href="oauth2-common-0.2-SNAPSHOT.jar"/>
+              <jar href="jettison-1.2.jar"/>
+              <jar href="slf4j-api-1.6.1.jar"/>
+              <jar href="httpclient-4.1.2.jar"/>
+              <jar href="httpcore-4.1.2.jar"/>
+              <jar href="commons-logging-1.1.1.jar"/>
+              <jar href="commons-codec-1.4.jar"/>
+              <jar href="log4j-1.2.16.jar"/>
+              <jar href="commons-lang-2.3.jar"/>
+              <jar href="commons-io-2.0.jar"/>
+              <jar href="xstream-1.4.2.jar"/>
+              <jar href="xmlpull-1.1.3.1.jar"/>
+              <jar href="xpp3_min-1.1.4c.jar"/>
+              <jar href="gson-1.7.1.jar"/>
+              <jar href="groovy-all-1.8.5.jar"/>
+              <jar href="woodstox-core-asl-4.0.9.jar"/>
+              <jar href="stax-api-1.0-2.jar"/>
+              <jar href="stax2-api-3.0.3.jar"/>
+              <jar href="jfreechart-1.0.13.jar"/>
+              <jar href="jcommon-1.0.16.jar"/>
+              <jar href="itext-2.1.7.jar"/>
+              <jar href="bcmail-jdk14-138.jar"/>
+              <jar href="bcprov-jdk14-138.jar"/>
+              <jar href="bctsp-jdk14-1.38.jar"/>
+              <jar href="bcprov-jdk14-1.38.jar"/>
+              <jar href="bcmail-jdk14-1.38.jar"/>
+              <jar href="stringtemplate-3.0.jar"/>
+              <jar href="antlr-2.7.7.jar"/>
+              <jar href="cglib-2.1_3.jar"/>
+              <jar href="asm-1.5.3.jar"/>
+          </resources>
           <application-desc main-class="eu.delving.sip.Application">
-            <argument>{user}</argument>
+            <argument>
+              {user}
+            </argument>
           </application-desc>
         </jnlp>
 
