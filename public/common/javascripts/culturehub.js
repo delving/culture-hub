bccb9b0384e490487e48db1b2b2e30c76ee937f4
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-07-09 19:29:04 +0200

    Cleanup

--- public/common/javascripts/culturehub.js
+++ public/common/javascripts/culturehub.js
@@ -680,39 +680,6 @@ function checkSimpleSearchSubmit(oId){
     return true;
 }
 
-$(document).ready(function() {
-
-//    // Toggle the dropdown menu's
-//    $(".dropdown .button, .dropdown button").click(function () {
-//        if (!$(this).find('span.toggle').hasClass('active')) {
-//            $('.dropdown-slider').slideUp();
-//            $('span.toggle').removeClass('active');
-//        }
-//
-//    // open selected dropown
-//        $(this).parent().find('.dropdown-slider').slideToggle('fast');
-//        $(this).find('span.toggle').toggleClass('active');
-//
-//        return false;
-//    });
-//
-//    // Launch TipTip tooltip
-//    $('.tiptip a.button, .tiptip button').tipTip();
-//
-//    $('select.link').change(function(){
-//       window.location.href=$(this).attr('value');
-//    });
-
-});
-
-// Close open dropdown slider by clicking elsewhwere on page
-//$(document).bind('click', function (e) {
-//    if (e.target.id != $('.dropdown').attr('class')) {
-//        $('.dropdown-slider').slideUp();
-//        $('span.toggle').removeClass('active');
-//    }
-//});
-
 
 /**
  * jQuery plugin for the dropbox
