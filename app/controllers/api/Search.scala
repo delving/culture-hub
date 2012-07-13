f1b23d2897e6b2838fce43cb5189a09ebad4e9fb
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-07-13 12:10:19 +0200

    Removing not-used search API

--- app/controllers/api/Search.scala
+++ app/controllers/api/Search.scala
@@ -30,18 +30,6 @@ object Search extends DelvingController {
             if (!orgId.isEmpty)
               hiddenQueryFilters += "%s:%s".format(ORG_ID, orgId)
 
-            if (provider.isDefined) {
-              hiddenQueryFilters += """%s:"%s"""".format(PROVIDER, provider.get.replaceAll("_", " "))
-            }
-
-            if (dataProvider.isDefined) {
-              hiddenQueryFilters += """%s:"%s"""".format(OWNER, dataProvider.get.replaceAll("_", " "))
-            }
-
-            if (collection.isDefined) {
-              hiddenQueryFilters += """%s:"%s"""".format(SPEC, collection.get)
-            }
-
             SearchService.getApiResult(Some(orgId), request, theme, hiddenQueryFilters.toList)
 
           } map {
