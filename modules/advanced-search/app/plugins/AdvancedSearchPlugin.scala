4b7adcf599920f7d0d7e1124e0e1f01ca9f5bd13
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-08-01 12:03:30 +0200

    Fixing plugin

--- modules/advanced-search/app/plugins/AdvancedSearchPlugin.scala
+++ modules/advanced-search/app/plugins/AdvancedSearchPlugin.scala
@@ -15,7 +15,7 @@ class AdvancedSearchPlugin(app: Application) extends CultureHubPlugin(app) {
 
   val pluginKey: String = "advanced-search"
   override val routes: ListMap[(String, Regex), (List[String]) => Handler] = ListMap(
-    ("GET", """^/search/advanced$""") -> {
+    ("GET", """^/search/advanced$""".r) -> {
       pathArgs: List[String] => controllers.search.AdvancedSearch.advancedSearch()
     }
   )
