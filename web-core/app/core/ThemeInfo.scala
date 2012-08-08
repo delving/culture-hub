170878ec4a464f11e1288b565ea200eec3868f83
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-08-08 16:03:23 +0200

    googleAnalyticsTrackingCode

--- web-core/app/core/ThemeInfo.scala
+++ web-core/app/core/ThemeInfo.scala
@@ -39,6 +39,7 @@ class ThemeInfo(configuration: DomainConfiguration) {
   val siteName = configuration.ui.siteName.getOrElse("Delving CultureHub")
   val siteSlogan = configuration.ui.siteSlogan.getOrElse("")
   val addThisUrl = configuration.ui.addThisTrackingCode.getOrElse("")
+  val googleAnalyticsTrackingCode = configuration.ui.googleAnalyticsTrackingCode.getOrElse("")
   val showLogin = configuration.ui.showLogin
 
 }
