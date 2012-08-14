fab8d5b2d0e3c107f6550ad78349d363cdeb46cf
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-08-14 16:49:43 +0200

    Advanced Search Form knet

--- modules/advanced-search/app/controllers/search/AdvancedSearch.scala
+++ modules/advanced-search/app/controllers/search/AdvancedSearch.scala
@@ -1,7 +1,9 @@
 package controllers.search
 
 import play.api.mvc._
+import play.api.Play.current
 import controllers.DelvingController
+import play.api.Play
 
 /**
  * Advanced search. This one is tailored from ICN and is taken from the legacy system.
@@ -14,7 +16,17 @@ object AdvancedSearch extends DelvingController {
   def advancedSearch = Root {
     Action {
       implicit request =>
-        Ok(Template('orgId -> configuration.orgId))
+
+        advancedSearchType.map { searchForm => searchForm match {
+          case "icn" =>
+            Ok(Template("/search/AdvancedSearch/advancedSearch1.html", 'orgId -> configuration.orgId))
+          case "kulturnett" =>
+            Ok(Template("/search/AdvancedSearch/advancedSearch2.html", 'orgId -> configuration.orgId))
+        }
+
+        }.getOrElse {
+          NotFound("Advanced search not configured")
+        }
     }
   }
 
@@ -24,24 +36,61 @@ object AdvancedSearch extends DelvingController {
 
         def field(name: String) = request.body.getFirst(name).getOrElse(null)
 
-        // TODO this is taken straight from the legacy code-base, and should be replaced by something more elegant
-        val form = new AdvancedSearchForm()
-        form.setFacet0(field("facet0"))
-        form.setValue0(field("value0"))
-        form.setOperator1(field("operator1"))
-        form.setFacet1(field("facet1"))
-        form.setValue1(field("value1"))
-        form.setOperator2(field("operator2"))
-        form.setFacet2(field("facet2"))
-        form.setValue2(field("value2"))
-        form.setAllOwners(field("allOwners"))
-        form.setOwnerList(request.body.get("ownersList").getOrElse(Seq.empty).toArray)
+        advancedSearchType.map {
+          searchForm =>
 
-        val query = form.toSolrQuery
+            val query = searchForm match {
 
-        Ok("/search?query=" + query)
+              case "icn" =>
+                val form = new AdvancedSearchForm1()
+                form.setFacet0(field("facet0"))
+                form.setValue0(field("value0"))
+                form.setOperator1(field("operator1"))
+                form.setFacet1(field("facet1"))
+                form.setValue1(field("value1"))
+                form.setOperator2(field("operator2"))
+                form.setFacet2(field("facet2"))
+                form.setValue2(field("value2"))
+                form.setAllOwners(field("allOwners"))
+                form.setOwnerList(request.body.get("ownersList").getOrElse(Seq.empty).toArray)
+
+                form.toSolrQuery
+
+              case "kulturnett" =>
+                val form = new AdvancedSearchForm2()
+                form.setFacet0(field("facet0"))
+                form.setValue0(field("value0"))
+                form.setOperator1(field("operator1"))
+                form.setFacet1(field("facet1"))
+                form.setValue1(field("value1"))
+                form.setOperator2(field("operator2"))
+                form.setFacet2(field("facet2"))
+                form.setValue2(field("value2"))
+                form.setAllOwners(field("allOwners") == "all")
+                form.setAllCounties(field("allCounties") == "all")
+                form.setAllMunicipalities(field("allMunicipalities") == "all")
+                form.setOwnerList(request.body.get("ownersList").getOrElse(Seq.empty).toArray)
+                form.setCountyList(request.body.get("countyList").getOrElse(Seq.empty).toArray)
+                form.setMunicipalityList(request.body.get("municipalityList").getOrElse(Seq.empty).toArray)
+                form.setOnlyDigitalObjects(field("onlyDigitalObjects").toBoolean)
+                form.setAllTypes(field("allTypes") == "all")
+                form.setTypeList(request.body.get("typeList").getOrElse(Seq.empty).toArray)
+
+                form.toSolrQuery
+
+
+            }
+
+            Ok("/search?query=" + query)
+
+
+        }.getOrElse {
+          NotFound("Advanced search not configured")
+        }
     }
 
   }
+
+  private def advancedSearchType = Play.configuration.getString("modules.advancedSearch.type", Some(Set("icn", "kulturnett")))
 
 }
