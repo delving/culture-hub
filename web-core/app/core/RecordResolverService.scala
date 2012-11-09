2fb44e2946355e3aebfe5b751f3399c142d32495
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-11-09 13:12:11 +0100

    RecordResolverService now gets access to the HTTP request

--- web-core/app/core/RecordResolverService.scala
+++ web-core/app/core/RecordResolverService.scala
@@ -3,6 +3,7 @@ package core
 import core.rendering.ViewType
 import eu.delving.schema.SchemaVersion
 import models.DomainConfiguration
+import play.api.mvc.RequestHeader
 
 /**
  *
@@ -16,7 +17,7 @@ trait RecordResolverService {
    * @param hubId the ID of the record
    * @param schemaVersion the (optional) version of the schema to be fetched
    */
-  def getRecord(hubId: HubId, schemaVersion: Option[SchemaVersion] = None)(implicit configuration: DomainConfiguration): Option[RenderableRecord]
+  def getRecord(hubId: HubId, schemaVersion: Option[SchemaVersion] = None)(implicit request: RequestHeader, configuration: DomainConfiguration): Option[RenderableRecord]
 
 }
 
