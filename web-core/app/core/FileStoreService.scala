b4500c93e0aadacc9e3465f1dec931f74a357f8b
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-12-04 19:50:25 +0100

    Introducing discreminator

--- web-core/app/core/FileStoreService.scala
+++ web-core/app/core/FileStoreService.scala
@@ -9,8 +9,13 @@ import java.io.File
  *
  * This trait describes the basic operations a FileStoreService needs to provide to be useful in the context of a hub.
  *
- * We use the notion of "bucket" to which a file belongs to. A bucket can e.g. be an item identifier, a collection identifier, etc.
- * A file also has a unique identifier by which it can be retrieved.
+ * We abstract over the traditional notion of file on a file-system and add means to keep the files organized by introducing the following concepts:
+ *
+ * - bucket: represents a place in which a file is stored. Buckets can be listed, renamed, removed.
+ * - fileType: an optional discriminator making it possible to arbitrarily classify files
+ * - unique file identifier: each file should have a unique identifier, making it possible to retrieve it via this identifier. The storage service
+ *   must ensure that the file identifier be constant over time (even if e.g. a bucket is renamed)
+ *
  *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
@@ -22,18 +27,20 @@ trait FileStoreService {
    * @param configuration the running [[ DomainConfiguration ]]
    * @return a sequence of [[ StoredFile ]]
    */
-  def listFiles(bucketId: String)(implicit configuration: DomainConfiguration): List[StoredFile]
+  def listFiles(bucketId: String, fileType: Option[String])(implicit configuration: DomainConfiguration): List[StoredFile]
 
   /**
    * Deletes all the files belonging to a bucket
    * @param bucketId the identifier of a bucket
+   * @param fileType an optional discriminator for the file
    * @param configuration the running [[ DomainConfiguration ]]
    */
-  def deleteFiles(bucketId: String)(implicit configuration: DomainConfiguration)
+  def deleteFiles(bucketId: String, fileType: Option[String])(implicit configuration: DomainConfiguration)
 
   /**
    * Deletes a single file
    * @param fileIdentifier the unique identifier of the file
+   * @param fileType an optional discriminator for the file
    * @param configuration the running [[ DomainConfiguration ]]
    */
   def deleteFile(bucketId: String, fileIdentifier: String)(implicit configuration: DomainConfiguration)
@@ -44,9 +51,10 @@ trait FileStoreService {
    * @param file the File
    * @param contentType the content-type of the file
    * @param fileName the file name this file should be given
+   * @param fileType an optional discriminator for the file
    * @return a [[ StoredFile ]] if the storage is successful, None otherwise
    */
-  def storeFile(bucketId: String, file: File, contentType: String, fileName: String): Option[StoredFile]
+  def storeFile(bucketId: String, file: File, contentType: String, fileName: String, fileType: Option[String]): Option[StoredFile]
 
   /**
    * Renames a bucket
