328b2e4dad81cf5ba512faeb4a97fc4285a0c646
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-12-20 11:58:32 +0100

    Fixes for client code of Processor

--- modules/dataset/app/jobs/Processor.scala
+++ modules/dataset/app/jobs/Processor.scala
@@ -28,7 +28,14 @@ class Processor extends Actor {
 
         if(currentState == DataSetState.PROCESSING_QUEUED) {
           DataSet.dao(set.orgId).updateState(set, DataSetState.PROCESSING)
-          DataSetCollectionProcessor.process(set)
+          DataSetCollectionProcessor.process(set, {
+            val state = DataSet.dao.getState(set.orgId, set.spec)
+            if(state == DataSetState.PROCESSING) {
+              DataSet.dao.updateState(set, DataSetState.ENABLED)
+            } else if(state == DataSetState.CANCELLED) {
+              DataSet.dao.updateState(set, DataSetState.UPLOADED)
+            }
+          })
         } else if(currentState != DataSetState.CANCELLED) {
           log.warn("Trying to process set %s which is not in PROCESSING_QUEUED state but in state %s".format(
             set.spec, currentState
