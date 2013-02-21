2d2046436d4311437e0f2bcde971672d77900130
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-02-21 10:01:45 +0100

    Preparation for runtime configuration reloading

--- test/PlatformSpec.scala
+++ test/PlatformSpec.scala
@@ -22,7 +22,7 @@ class PlatformSpec extends Specs2TestContext {
     "load configurations from disk into memory" in {
       withTestConfig {
 
-        organizationConfigurationHandler.startup(CultureHubPlugin.hubPlugins)
+        organizationConfigurationHandler.configure(CultureHubPlugin.hubPlugins)
         val (configurations, errors) = OrganizationConfiguration.buildConfigurations(Play.application.configuration, CultureHubPlugin.hubPlugins)
         configurations.size should not equalTo (0)
         errors.size should equalTo (0)
