32a6d3aad3281d2534a242ccb2de9cc0109b407d
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-03-27 18:00:39 +0100

    13.04-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object Build extends sbt.Build {
   val cultureHubPath = ""
 
   val appName = "culture-hub"
-  val cultureHubVersion = "13.03-SNAPSHOT"
+  val cultureHubVersion = "13.04-SNAPSHOT"
   val sipAppVersion = "1.1.1-SNAPSHOT"
   val sipCoreVersion = "1.1.1-SNAPSHOT"
   val schemaRepoVersion = "1.1.2-SNAPSHOT"
