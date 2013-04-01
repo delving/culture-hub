9af774ed5a4126362df332a9679f2e76ef8b69d0
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-01 17:35:28 +0200

    Just in case that we need it to be around

---
+++ app/controllers/organizations/SimpleDocumentUpload.scala
@@ -0,0 +1,310 @@
+package controllers.organizations
+
+import play.api.mvc._
+import models._
+import plugins.SimpleDocumentUploadPlugin
+import controllers.{ BoundController, OrganizationController }
+import core._
+import extensions.{ MissingLibs, JJson }
+import org.bson.types.ObjectId
+import play.api.i18n.Messages
+import play.api.data.Form
+import play.api.data.Forms._
+import org.apache.solr.common.SolrInputDocument
+import core.indexing.IndexField._
+import core.indexing.IndexField
+import models.MetadataItem
+import models.MetadataCache
+import play.api.data.Forms.mapping
+import storage.{ FileStorage, FileUploadResponse, StoredFile }
+import controllers.dos.FileUpload
+import scala.collection.mutable
+
+/**
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+object SimpleDocumentUpload extends BoundController(HubModule) with SimpleDocumentUpload
+
+trait SimpleDocumentUpload extends OrganizationController { this: BoundController =>
+
+  val schemaService = inject[SchemaService]
+
+  def list(orgId: String) = OrganizationMember {
+    Action {
+      implicit request =>
+        withAccess {
+
+          val items = cache.list().map { item =>
+            ListItemViewModel(
+              id = item.itemId,
+              name = item.getSystemFieldValues(SystemField.TITLE).headOption.getOrElse("Unknown"),
+              description = item.getSystemFieldValues(SystemField.DESCRIPTION).headOption.getOrElse("")
+            )
+          }
+
+          val itemsAsJson = JJson.generate(Map("documents" -> items))
+
+          Ok(Template('data -> itemsAsJson))
+        }
+    }
+  }
+
+  def simpleDocumentUpload(orgId: String, id: Option[String]) = OrganizationMember {
+    Action {
+      implicit request =>
+        withAccess {
+
+          val config = SimpleDocumentUploadPlugin.pluginConfiguration
+
+          id.map { itemId =>
+            cache.findOne(itemId).map { item =>
+
+              item.xml.get(config.schemaPrefix).map { rawXml =>
+
+                try {
+                  val document = scala.xml.XML.loadString(rawXml)
+
+                  val fields: Seq[Field] = config.fields.flatMap { field =>
+                    val Array(prefix, label) = field.key.split(":")
+                    val maybeField = document.child.filter(node => node.prefix == prefix && node.label == label)
+                    val existing = maybeField.map { fieldValue =>
+                      Field(field.key, field.fieldType, Messages("metadata." + field.key.replaceAll(":", ".")), fieldValue.text, field.hasOptions, field.options)
+                    }
+                    val delta = field.multiplicity - maybeField.length
+                    val additional = if (delta >= 0) {
+                      for (i <- 0 until delta) yield {
+                        Field(field.key, field.fieldType, Messages("metadata." + field.key.replaceAll(":", ".")), "", field.hasOptions, field.options)
+                      }
+                    } else {
+                      Seq.empty
+                    }
+                    existing ++ additional
+                  }
+
+                  val files = core.storage.FileStorage.listFiles(itemId).map(f => FileUploadResponse(f))
+
+                  val itemViewModel = ItemViewModel(item.itemId, fields, files)
+                  val itemViewModelAsJson = JJson.generate(itemViewModel)
+
+                  Ok(Template('data -> itemViewModelAsJson, 'uid -> MissingLibs.UUID, 'id -> itemId))
+
+                } catch {
+                  case t: Throwable =>
+                    Error("Could not parse content for item " + itemId, t)
+                }
+
+              }.getOrElse {
+                Error("Could not find document content for item " + itemId)
+              }
+
+            }.getOrElse {
+              NotFound("Item with ID %s was not found".format(id))
+            }
+          }.getOrElse {
+            val generatedId = "%s_%s_%s".format(orgId, config.collectionName, new ObjectId().toString)
+            val itemViewModel = ItemViewModel(
+              generatedId,
+              config.fields.flatMap { f =>
+                for (i <- 0 until f.multiplicity) yield {
+                  Field(f.key, f.fieldType, Messages("metadata." + f.key.replaceAll(":", ".")), "", f.hasOptions, f.options)
+                }
+              }
+            )
+            val itemViewModelAsJson = JJson.generate(itemViewModel)
+
+            Ok(Template('data -> itemViewModelAsJson, 'uid -> MissingLibs.UUID, 'id -> generatedId))
+          }
+
+        }
+    }
+  }
+
+  def submit(orgId: String) = OrganizationMember {
+    Action {
+      implicit request =>
+        withAccess {
+
+          val config = SimpleDocumentUploadPlugin.pluginConfiguration
+
+          ItemViewModel.itemForm.bind(request.body.asJson.get).fold(
+            formWithErrors => handleValidationError(formWithErrors),
+            itemModel => {
+
+              val index: Int = cache.findOne(itemModel.id).map { item =>
+                item.index
+              }.getOrElse {
+                cache.count().toInt
+              }
+
+              // handle attached files
+              val files: Seq[StoredFile] = FileStorage.listFiles(itemModel.id)
+
+              val (pdfs, images) = files.partition(_.contentType == "application/pdf")
+
+              val pdfUrls = pdfs.map { p =>
+                "http://" + request.host + "/file/" + p.id.toString
+              }
+              val imageUrls = images.map { i =>
+                "http://" + request.host + "/image/" + i.id.toString
+              }
+              val thumbnailUrls = (images ++ pdfs).map { t =>
+                "http://" + request.host + "/thumbnail/" + t.id.toString
+              }
+              val pdfFields = pdfUrls.map { p =>
+                """<%s>%s</%s>""".format(
+                  IndexField.FULL_TEXT_OBJECT_URL.xmlKey, p, IndexField.FULL_TEXT_OBJECT_URL.xmlKey
+                )
+              }
+              val imageFields = imageUrls.map { i =>
+                """<%s>%s</%s>""".format(
+                  SystemField.IMAGE_URL.xmlKey, i, SystemField.IMAGE_URL.xmlKey
+                )
+              }
+              val thumbnailFields = thumbnailUrls.map { t =>
+                """<%s>%s</%s>""".format(
+                  SystemField.THUMBNAIL.xmlKey, t, SystemField.THUMBNAIL.xmlKey
+                )
+              }
+              val fields = itemModel.fields.map { f =>
+                """<%s>%s</%s>""".format(f.key, f.value, f.key)
+              }
+
+              val titleField: String = itemModel.fields.find(_.key == config.titleField).map(_.value).getOrElse("")
+
+              val automaticFields = Seq(SystemField.TITLE).map { f =>
+                """<%s>%s</%s>""".format(f.xmlKey, titleField, f.xmlKey)
+              }
+
+              val recordDefinition = RecordDefinition.getRecordDefinition(config.schemaPrefix, config.schemaVersion).getOrElse {
+                throw new RuntimeException("This shouldn't logically happen")
+              }
+
+              val xml = """<%s:record %s>%s</%s:record>""".format(
+                config.schemaPrefix,
+                util.XMLUtils.namespacesToString(recordDefinition.allNamespaces),
+                (fields ++ automaticFields ++ imageFields ++ thumbnailFields ++ pdfFields).mkString,
+                config.schemaPrefix
+              )
+
+              val item = MetadataItem(
+                collection = config.collectionName,
+                itemType = SimpleDocumentUploadPlugin.ITEM_TYPE.itemType,
+                itemId = itemModel.id,
+                xml = Map(config.schemaPrefix -> xml),
+                schemaVersions = Map(config.schemaPrefix -> config.schemaVersion),
+                systemFields = Map(
+                  SystemField.TITLE.tag -> List(titleField),
+                  SystemField.THUMBNAIL.tag -> thumbnailUrls.toList
+                ),
+                index = index
+              )
+
+              cache.saveOrUpdate(item)
+
+              val solrDocument = new mutable.HashMap[String, mutable.Set[Any]] with mutable.MultiMap[String, Any]
+
+              itemModel.fields.foreach { f =>
+                val typedFieldKey = "%s_%s".format(f.key.replaceAll(":", "_"), f.fieldType)
+                solrDocument.addBinding(typedFieldKey, f.value)
+              }
+              // TODO hack, solr type should be read from rec-def, then again it is a systemField...
+              pdfUrls.foreach { p =>
+                solrDocument.addBinding(IndexField.FULL_TEXT_OBJECT_URL.key + "_link", p)
+              }
+              imageUrls.foreach { i =>
+                solrDocument.addBinding(SystemField.IMAGE_URL.tag, i)
+              }
+              thumbnailUrls.foreach { t =>
+                solrDocument.addBinding(SystemField.THUMBNAIL.tag, t)
+              }
+
+              solrDocument.addBinding(SystemField.TITLE.tag, titleField)
+
+              solrDocument += (ID -> itemModel.id)
+              solrDocument += (HUB_ID -> itemModel.id)
+              solrDocument += (ORG_ID -> orgId)
+              solrDocument += (RECORD_TYPE -> SimpleDocumentUploadPlugin.ITEM_TYPE.itemType)
+
+              indexingServiceLocator.byDomain.stageForIndexing(solrDocument.toMap)
+              indexingServiceLocator.byDomain.commit
+
+              Json(itemModel)
+            })
+        }
+    }
+  }
+
+  def delete(orgId: String, id: String) = OrganizationMember {
+    Action {
+      implicit request =>
+        withAccess {
+          cache.remove(id)
+          indexingServiceLocator.byDomain.deleteById(id)
+          FileStorage.deleteFiles(id)
+          Ok
+        }
+    }
+  }
+
+  def upload(orgId: String, uid: String, id: String) = OrganizationMember {
+    Action {
+      implicit request =>
+        withAccess {
+          FileUpload.markFilesAttached(uid, id)
+          Ok
+        }
+    }
+  }
+
+  case class ListItemViewModel(id: String, name: String, description: String)
+
+  case class ItemViewModel(id: String, fields: Seq[Field], files: Seq[FileUploadResponse] = Seq.empty)
+
+  object ItemViewModel {
+
+    val itemForm = Form(
+      mapping(
+        "id" -> nonEmptyText,
+        "fields" -> seq(
+          mapping(
+            "key" -> nonEmptyText,
+            "fieldType" -> nonEmptyText,
+            "label" -> nonEmptyText,
+            "value" -> text,
+            "hasOptions" -> boolean,
+            "options" -> seq(text)
+          )(Field.apply)(Field.unapply)
+        ),
+        "files" -> seq(
+          mapping(
+            "name" -> text,
+            "size" -> longNumber,
+            "url" -> text,
+            "thumbnail_url" -> text,
+            "delete_url" -> text,
+            "delete_type" -> text,
+            "error" -> text,
+            "id" -> text
+          )(FileUploadResponse.apply)(FileUploadResponse.unapply))
+      )(ItemViewModel.apply)(ItemViewModel.unapply)
+    )
+
+  }
+
+  case class Field(key: String, fieldType: String, label: String, value: String, hasOptions: Boolean, options: Seq[String])
+
+  private def withAccess(block: => Result)(implicit request: RequestHeader, configuration: OrganizationConfiguration): Result = {
+    if (Group.dao.hasAnyRole(connectedUser, Seq(SimpleDocumentUploadPlugin.ROLE_DOCUMENT_EDITOR, Role.OWN))) {
+      block
+    } else {
+      Forbidden
+    }
+  }
+
+  private def cache(implicit configuration: OrganizationConfiguration) = {
+    val collectionName = SimpleDocumentUploadPlugin.pluginConfiguration.collectionName
+    MetadataCache.get(configuration.orgId, collectionName, SimpleDocumentUploadPlugin.ITEM_TYPE)
+  }
+
+}
---
+++ test/SimpleDocumentUploadSpec.scala
@@ -0,0 +1,169 @@
+import core.SystemField
+import java.io.File
+import models.MetadataCache
+import org.apache.solr.client.solrj.SolrQuery
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import play.api.libs.json.JsArray
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.mvc.AnyContentAsJson
+import play.api.test.Helpers._
+import play.api.test.{ FakeHeaders, FakeRequest }
+import plugins.SimpleDocumentUploadPlugin
+import test.TestContext
+import util.OrganizationConfigurationHandler
+import xml.XML
+import services.search.SolrQueryService
+
+/**
+ *
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+class SimpleDocumentUploadSpec extends FlatSpec with ShouldMatchers with TestContext {
+
+  "The SimpleDocumentUpload" should "submit and store a document" in {
+
+    withTestData() {
+
+      implicit val configuration = OrganizationConfigurationHandler.getByOrgId("delving")
+
+      val result = controllers.organizations.SimpleDocumentUpload.submit("delving")(fakeRequest)
+      status(result) should equal(OK)
+
+      val maybeDoc = MetadataCache.get("delving", "uploadDocuments", "uploadDocument").findOne("delving_uploadDocuments_503e203903643da47461306e")
+      maybeDoc should not equal (None)
+      val doc = maybeDoc.get
+
+      doc.getSystemFieldValues(SystemField.TITLE).headOption should equal(Some("Sample title"))
+      doc.index should equal(0)
+      doc.schemaVersions.get("tib") should equal(Some("1.0.1"))
+      doc.xml("tib") should equal("""<tib:record xmlns:europeana="http://www.europeana.eu/schemas/ese/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:tib="http://www.tib.nl/schemas/tib/" xmlns:delving="http://schemas.delving.eu/"><dc:title>Sample title</dc:title><dc:subject>Random subject</dc:subject><delving:title>Sample title</delving:title></tib:record>""")
+    }
+
+  }
+
+  "The SimpleDocumentUpload" should "submit and index a document" in {
+    withTestData() {
+
+      implicit val configuration = OrganizationConfigurationHandler.getByOrgId("delving")
+
+      val result = controllers.organizations.SimpleDocumentUpload.submit("delving")(fakeRequest)
+      status(result) should equal(OK)
+
+      val queryById = SolrQueryService.getSolrResponseFromServer(new SolrQuery("delving_orgId:delving id:delving_uploadDocuments_503e203903643da47461306e"))
+      queryById.getResults.size() should equal(1)
+    }
+  }
+
+  "The SimpleDocumentUpload" should "propertly integrate uploaded files into a document" in {
+
+    withTestData() {
+
+      import play.api.Play.current
+
+      implicit val configuration = OrganizationConfigurationHandler.getByOrgId("delving")
+
+      indexingServiceLocator.byDomain(configuration).deleteByQuery("*:*")
+
+      val pdf = new File(current.path, "../modules/simple-document-upload/conf/sample.pdf")
+      val png = new File(current.path, "public/images/dummy-object.png")
+      val uid = "123456"
+
+      val (f, thumbnailUrl) = controllers.dos.FileUpload.storeFile(pdf, "sample.pdf", "application/pdf", uid).get
+      val (f1, thumbnailUrl1) = controllers.dos.FileUpload.storeFile(png, "delving-team.jpg", "image/jpg", uid).get
+
+      val simulatedAttachment = controllers.organizations.SimpleDocumentUpload.upload("delving", uid, "delving_uploadDocuments_503e203903643da47461306e")(
+        FakeRequest().withSession(
+          ("userName" -> "bob")
+        ))
+
+      status(simulatedAttachment) should equal(OK)
+
+      val result = controllers.organizations.SimpleDocumentUpload.submit("delving")(fakeRequest)
+      status(result) should equal(OK)
+
+      val maybeDoc = MetadataCache.get("delving", "uploadDocuments", SimpleDocumentUploadPlugin.ITEM_TYPE).findOne("delving_uploadDocuments_503e203903643da47461306e")
+      maybeDoc should not equal (None)
+      val doc = maybeDoc.get
+
+      doc.xml.get("tib") should not equal (None)
+      val content = doc.xml("tib")
+
+      val parsed = XML.loadString(content)
+
+      (parsed \ "title").find(_.prefix == "delving").map(_.text) should equal(Some("Sample title"))
+      (parsed \ "title").find(_.prefix == "dc").map(_.text) should equal(Some("Sample title"))
+      (parsed \ "subject").text should equal("Random subject")
+      (parsed \ "imageUrl").text should equal("http:///image/" + f1.id.toString)
+      (parsed \ "thumbnail").map(_.text + "/80") should equal(Seq("http://" + thumbnailUrl1, "http://" + thumbnailUrl))
+      (parsed \ "fullTextObjectUrl").text should equal("http:///file/" + f.id.toString)
+
+      val queryById = SolrQueryService.getSolrResponseFromServer(new SolrQuery("delving_orgId:delving id:delving_uploadDocuments_503e203903643da47461306e"))
+      queryById.getResults.size() should equal(1)
+
+      val solrDocument = queryById.getResults.get(0)
+
+      solrDocument.getFirstValue("delving_fullTextObjectUrl_link") should equal("http:///file/" + f.id.toString)
+      solrDocument.getFirstValue("delving_imageUrl") should equal("http:///image/" + f1.id.toString)
+      solrDocument.getFirstValue("delving_thumbnail") should equal("http:///thumbnail/" + f1.id.toString)
+      solrDocument.getFirstValue("delving_hasDigitalObject") should equal(true)
+
+    }
+
+  }
+
+  // re-enable this if this plugin gets ever used seriously
+  //
+  //  "The SimpleDocumentUpload" should "index a PDF as full text with Tika" in {
+  //    withTestData() {
+  //
+  //      import play.api.Play.current
+  //
+  //      implicit val configuration = OrganizationConfigurationHandler.getByOrgId("delving")
+  //
+  //      val pdf = new File(current.path, "../modules/simple-document-upload/conf/sample.pdf")
+  //      val uid = "123456"
+  //
+  //      val (f, thumbnailUrl) = controllers.dos.FileUpload.storeFile(pdf, "sample.pdf", "application/pdf", uid).get
+  //
+  //      val simulatedAttachment = controllers.organizations.SimpleDocumentUpload.upload("delving", uid, "delving_uploadDocuments_503e203903643da47461306e")(
+  //        FakeRequest().withSession(
+  //          ("userName" -> "bob")
+  //        ))
+  //
+  //      status(simulatedAttachment) should equal(OK)
+  //
+  //      val result = controllers.organizations.SimpleDocumentUpload.submit("delving")(fakeRequest)
+  //      status(result) should equal(OK)
+  //
+  //      val queryFullText = SolrQueryService.getSolrResponseFromServer(new SolrQuery("Anticonstitutionellement"))
+  //      // FIXME this does not work yet because due to how the Tika indexing is implemented, the remote fetching in the test scope without real URL does not work
+  //      //      queryFullText.getResults.size() should equal(1)
+  //    }
+  //  }
+
+  def fakeRequest = FakeRequest(
+    method = "POST",
+    uri = "http://delving.localhost:9000",
+    headers = FakeHeaders(Seq(CONTENT_TYPE -> Seq("application/json"))),
+    body = AnyContentAsJson(
+      JsObject(
+        Seq(
+          "id" -> JsString("delving_uploadDocuments_503e203903643da47461306e"),
+          "fields" -> JsArray(
+            Seq(
+              JsObject(Seq("key" -> JsString("dc:title"), "fieldType" -> JsString("text"), "label" -> JsString("Title"), "value" -> JsString("Sample title"))),
+              JsObject(Seq("key" -> JsString("dc:subject"), "fieldType" -> JsString("text"), "label" -> JsString("metadata.dc.subject"), "value" -> JsString("Random subject")))
+            )
+          ),
+          "files" -> JsArray(Seq.empty)
+        )
+      )
+    )
+  ).withSession(
+      ("userName" -> "bob")
+    )
+
+}
+
