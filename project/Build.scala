c94c5d1caa296e7e113613506b63e7de2dc0ff79
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-04 17:12:47 +0200

    Playing around with Swagger

--- project/Build.scala
+++ project/Build.scala
@@ -58,6 +58,8 @@ object Build extends sbt.Build {
 
     "org.scalesxml"             %% "scales-xml"                      % "0.4.4",
 
+    "com.wordnik"               %% "swagger-play2"                   % "1.2.1-SNAPSHOT",
+
     "org.scalatest"             %% "scalatest"                       % "2.0.M5b"             % "test",
 
     // temporary until https://play.lighthouseapp.com/projects/82401-play-20/tickets/970-xpathselecttext-regression is fixed
