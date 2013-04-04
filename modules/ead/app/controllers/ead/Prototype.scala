a2a572118a369abf3ffb638a623053d8971553c3
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-04 15:06:31 +0200

    Limiting tree rendering depth, lazy loading seems to work

--- modules/ead/app/controllers/ead/Prototype.scala
+++ modules/ead/app/controllers/ead/Prototype.scala
@@ -72,9 +72,10 @@ object Prototype extends DelvingController {
   }
 
   def transformNode(title: String, value: JValue, path: Stack[String], depth: Int, key: Option[String], subtree: ArrayBuffer[JValue]): JValue = {
+    val pathMatched = key != None && key.get == path.reverse.mkString
     val v = value match {
       case JObject(fields: Seq[JField]) =>
-        JObject(List(
+        val baseFields = List(
           JField(
             name = "title",
             value = JString(title)
@@ -86,7 +87,17 @@ object Prototype extends DelvingController {
           JField(
             name = "key",
             value = JString(path.reverse.mkString + "/" + title)
-          ),
+          )
+        )
+        val renderedFields = if (depth > 0 && (key.isEmpty || (key.isDefined && !subtree.isEmpty))) {
+          baseFields ::: List(
+            JField(
+              name = "lazy",
+              value = JBool(depth > 0)
+            )
+          )
+
+        } else baseFields ::: List(
           JField(
             name = "children",
             value = JArray(
@@ -96,10 +107,11 @@ object Prototype extends DelvingController {
               }
             )
           )
-        ))
+        )
+        JObject(renderedFields)
       case JArray(values: Seq[JValue]) =>
         JArray(values.zipWithIndex.map { v =>
-          val node = transformNode(title, v._1, path push (s"/$title[${v._2}]"), depth, key, subtree)
+          val node = transformNode(title, v._1, path push (s"/$title[${v._2}]"), depth + 1, key, subtree)
           node
         })
       case JString(s) => JObject(List(
@@ -123,7 +135,7 @@ object Prototype extends DelvingController {
 
       ))
     }
-    if (key != None && key.get == path.reverse.mkString) subtree append v
+    if (pathMatched) subtree append v
     v
   }
 
