b5a5824aadf619427d52425f4a54044fed11733b
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-04 14:36:43 +0200

    Key-based lookup for subtrees

--- modules/ead/app/controllers/ead/Prototype.scala
+++ modules/ead/app/controllers/ead/Prototype.scala
@@ -7,6 +7,8 @@ import play.api.Play.current
 import scala.xml._
 import net.liftweb.json._
 import collection.immutable.Stack
+import collection.mutable
+import collection.mutable.ArrayBuffer
 
 /**
  *
@@ -43,7 +45,7 @@ object Prototype extends DelvingController {
             val source = Source.fromInputStream(resourceStream)
             val xml = scala.xml.XML.load(source)
             val json = Xml.toJson(xml)
-            val transformed = transformToDynatree(json)
+            val transformed = transformToViewTree(json, request.queryString.getFirst("path"))
 
             Ok(pretty(net.liftweb.json.render(transformed))).as(JSON)
           }
@@ -53,22 +55,54 @@ object Prototype extends DelvingController {
 
   }
 
-  def transformToDynatree(json: JValue): JValue = {
+  def transformToViewTree(json: JValue, key: Option[String]): JValue = {
     json match {
       case o @ JObject(fields: Seq[JField]) =>
         val root = fields.head
+        val subtree = ArrayBuffer[JValue]()
+        val transformed = transformNode(root.name, root.value, Stack(), 0, key, subtree) match {
+          case JObject(fields: Seq[JField]) => List(JObject(fields))
+        }
+
         JArray(
-          transformNode(root.name, root.value, Stack(), 0) match {
-            case JObject(fields: Seq[JField]) => List(JObject(fields))
-          }
+          if (key.isDefined && !subtree.isEmpty) subtree.toList else transformed
         )
     }
 
   }
 
-  def transformNode(title: String, value: JValue, path: Stack[String], depth: Int): JValue = value match {
-    case JObject(fields: Seq[JField]) =>
-      JObject(List(
+  def transformNode(title: String, value: JValue, path: Stack[String], depth: Int, key: Option[String], subtree: ArrayBuffer[JValue]): JValue = {
+    val v = value match {
+      case JObject(fields: Seq[JField]) =>
+        JObject(List(
+          JField(
+            name = "title",
+            value = JString(title)
+          ),
+          JField(
+            name = "folder",
+            value = JBool(value = true)
+          ),
+          JField(
+            name = "key",
+            value = JString(path.reverse.mkString + "/" + title)
+          ),
+          JField(
+            name = "children",
+            value = JArray(
+              fields map { field =>
+                val node = transformNode(field.name, field.value, path push ("/" + title), depth + 1, key, subtree)
+                node
+              }
+            )
+          )
+        ))
+      case JArray(values: Seq[JValue]) =>
+        JArray(values.zipWithIndex.map { v =>
+          val node = transformNode(title, v._1, path push (s"/$title[${v._2}]"), depth, key, subtree)
+          node
+        })
+      case JString(s) => JObject(List(
         JField(
           name = "title",
           value = JString(title)
@@ -78,44 +112,19 @@ object Prototype extends DelvingController {
           value = JBool(value = true)
         ),
         JField(
-          name = "key",
-          value = JString(path.reverse.mkString)
-        ),
-        JField(
           name = "children",
-          value = JArray(
-            fields map { field =>
-              val node = transformNode(field.name, field.value, path push ("/" + title), depth + 1)
-              node
-            }
-          )
+          value = JArray(List(JObject(List(
+            JField(
+              name = "title",
+              value = JString(s)
+            )
+          ))))
         )
-      ))
-    case JArray(values: Seq[JValue]) =>
-      JArray(values.zipWithIndex.map { v =>
-        val node = transformNode(title, v._1, path push (s"/$title[${v._2}]"), depth)
-        node
-      })
-    case JString(s) => JObject(List(
-      JField(
-        name = "title",
-        value = JString(title)
-      ),
-      JField(
-        name = "folder",
-        value = JBool(value = true)
-      ),
-      JField(
-        name = "children",
-        value = JArray(List(JObject(List(
-          JField(
-            name = "title",
-            value = JString(s)
-          )
-        ))))
-      )
 
-    ))
+      ))
+    }
+    if (key != None && key.get == path.reverse.mkString) subtree append v
+    v
   }
 
 }
