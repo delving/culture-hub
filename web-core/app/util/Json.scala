a12802ce954de07f9b6c6fb084fefc3010d893d8
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-08 16:15:29 +0200

    First iteration of simplified tree, and interesting lift-json discoveries

--- web-core/app/util/Json.scala
+++ web-core/app/util/Json.scala
@@ -18,25 +18,17 @@ object Json {
    * @param sequences paths of objects that should only contain arrays
    * @return a string formatted as JSON
    */
-  def toJson(xml: NodeSeq, escapeNamespaces: Boolean = false, sequences: Seq[List[String]] = List.empty): String = {
+  def toJson(xml: NodeSeq, escapeNamespaces: Boolean = false, sequences: Seq[List[String]] = List.empty): JValue = {
 
     // because lift-json doesn't group XML nodes that have the same name under an array if there's more than one potential group, we do it ourselves here
-    val json = Xml.toJson(xml) transform {
-      case JObject(obj: List[JField]) => {
-        val capturedOrder = obj.map(_.name).zipWithIndex.toMap[String, Int]
-        val groupedFields = obj.groupBy(_.name)
-        val grouped = groupedFields map {
-          g =>
-            if (g._2.length > 1) {
-              JField(g._1, JArray(g._2.map(f => f.value)))
-            } else {
-              JField(g._1, g._2.head.value)
-            }
-        }
-        val reordered = grouped.toList.sortWith((one, two) => capturedOrder(one.name) < capturedOrder(two.name))
-        JObject(reordered)
-      }
-    }
+    // NOTE: lift-json is flattening leaf elements, i.e. considering
+    // <a><foo id="1">x</foo></a> -> {"a":{"foo":{"foo":"x","id":"1"}}} vs
+    // <a><foo id="1">x</foo></a> -> {"a":{"foo":"x","id":"1"}}
+    // the second case happens.
+    // this is leading to trouble if you've got more than one "id" on the same level, e.g.
+    // <a><foo id="1">x</foo><bar id="2">bla</bar></a> -> {"a":{"foo":"x","bar":"bla","id":["1","2"]}}
+    // actually in the original transformation, "1" gets lost, thanks to our systematic grouping we keep it - but it is confusing nonetheless
+    val json = groupingTraversal(Xml.toJson(xml))
 
     // when we are given a set of paths of JObjects for which all children should have arrays as values, mutate accordingly
     var mutated = json
@@ -58,7 +50,29 @@ object Json {
       mutated
     }
 
-    compact(render(js))
+    js
+  }
+
+  def renderToJson(xml: NodeSeq, escapeNamespaces: Boolean = false, sequences: Seq[List[String]] = List.empty): String = {
+    compact(render(toJson(xml, escapeNamespaces, sequences)))
+
+  }
+
+  private def groupingTraversal(node: JValue): JValue = node match {
+    case JObject(obj: Seq[JField]) =>
+      val capturedOrder = obj.map(_.name).zipWithIndex.toMap[String, Int]
+      val groupedFields = obj.groupBy(_.name)
+      val grouped = groupedFields map {
+        g =>
+          if (g._2.length > 1) {
+            JField(g._1, JArray(g._2.map(f => f.value)).map(groupingTraversal(_)))
+          } else {
+            JField(g._1, g._2.head.value.map(groupingTraversal(_)))
+          }
+      }
+      val reordered = grouped.toList.sortWith((one, two) => capturedOrder(one.name) < capturedOrder(two.name))
+      JObject(reordered)
+    case other @ _ => other
   }
 
 }
