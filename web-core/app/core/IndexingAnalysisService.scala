d154db389331f77a0c14b17c011f1301ef9d5be1
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-04-11 08:18:19 +0200

    First take at indexing

--- web-core/app/core/IndexingAnalysisService.scala
+++ web-core/app/core/IndexingAnalysisService.scala
@@ -2,6 +2,9 @@ package core
 
 import org.w3c.dom.Node
 import eu.delving.schema.SchemaVersion
+import scala.xml.parsing.NoBindingFactoryAdapter
+import com.sun.org.apache.xalan.internal.xsltc.trax.DOM2SAX
+import scala.collection.mutable
 
 /**
  * Analyzes a document to be indexed.
@@ -23,5 +26,21 @@ trait IndexingAnalysisService {
    * @return one or more [[ core.IndexingService#IndexDocument ]] ready to be processed further and sent for indexing
    */
   def analyze(hubId: HubId, schemaVersion: SchemaVersion, document: Node): Seq[IndexingService#IndexDocument]
+
+  /**
+   * Convenience function to turn a W3C DOM into a Scala DOM - useful for implementations of this service
+   */
+  def asXml(dom: Node): scala.xml.Node = {
+    val dom2sax = new DOM2SAX(dom)
+    val adapter = new NoBindingFactoryAdapter
+    dom2sax.setContentHandler(adapter)
+    dom2sax.parse()
+    adapter.rootElem
+  }
+
+  /**
+   * Convenience function to create a Map that can be turned into an IndexDocument by calling toMap
+   */
+  def newMultiMap = new mutable.HashMap[String, mutable.Set[Any]] with mutable.MultiMap[String, Any]
 
 }
