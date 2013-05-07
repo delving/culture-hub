056799ce8450caf4038871e67c53b49a64e190cc
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-05-07 11:59:20 +0200

    Automatically retrying to connect to the server in case of WebSocket disconnection. #879

--- public/common/javascripts/organizations/datasets.js
+++ public/common/javascripts/organizations/datasets.js
@@ -31,3 +31,31 @@ function stateToClass(state) {
     }
     return stateClass;
 }
+
+/**
+ * Handles WebSocket connection to DataSet feed
+ * @returns {number} the clientId of the connection, necessary for communicating with the server
+ */
+function feed(orgId, spec, onopen, onmessage, connectionRetries) {
+    var localRetries = connectionRetries;
+    var clientId = Math.floor(1 + 10001 * Math.random());
+    var url = "ws://" + location.host + "/organizations/" + orgId + "/dataset/feed?clientId=" + clientId + '&spec=' + spec;
+    ws = new WebSocket(url);
+    ws.onopen = function() {
+        localRetries = 0;
+        onopen();
+    };
+    ws.onmessage = onmessage;
+    ws.onclose = function() {
+        if (connectionRetries < 3) {
+            console.log("WebSocket disconnected, attempting to reconnect, attempt " + connectionRetries);
+            setTimeout(function() { feed(orgId, spec, onopen, onmessage, localRetries + 1) }, 5000);
+        } else {
+            // TODO here we might want to retry, but with a bigger timeout
+            // also instead of a bootbox alert, show a banner
+            bootbox.alert("Lost connection with the server. Please refresh the page.");
+
+        }
+    };
+    return clientId;
+}
