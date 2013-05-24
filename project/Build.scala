9364e8c9621bebdaf06536ea1a7ef397aced0bd9
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-05-24 11:37:48 +0200

    13.05-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object Build extends sbt.Build {
   val sipCore    = SettingKey[String]("sip-core", "Version of the SIP-Core")
   val schemaRepo = SettingKey[String]("schema-repo", "Version of the Schema Repository")
 
-  val cultureHubVersion = "13.04"
+  val cultureHubVersion = "13.05-SNAPSHOT"
   val sipAppVersion = "1.1.4"
   val sipCoreVersion = "1.1.4"
   val schemaRepoVersion = "1.1.4"
