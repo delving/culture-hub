c0463e3f242762d69c635d4a4fc22e4f38cf7121
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2013-06-20 11:05:14 +0200

    Admin navigation

--- modules/namedSlices/app/plugins/NamedSlicesPlugin.scala
+++ modules/namedSlices/app/plugins/NamedSlicesPlugin.scala
@@ -2,11 +2,23 @@ package plugins
 
 import play.api.Application
 import core.{ MenuElement, MainMenuEntry, CultureHubPlugin }
-import models.{ NamedSlice, OrganizationConfiguration }
+import models.{ Role, NamedSlice, OrganizationConfiguration }
 
 class NamedSlicesPlugin(app: Application) extends CultureHubPlugin(app) {
 
   val pluginKey: String = "namedSlices"
+
+  override def organizationMenuEntries(configuration: OrganizationConfiguration, lang: String, roles: Seq[String]): Seq[MainMenuEntry] = Seq(
+    MainMenuEntry(
+      key = "namedSlice",
+      titleKey = "namedslice.namedSlices",
+      roles = Seq(Role.OWN),
+      items = Seq(
+        MenuElement("/admin/namedSlices", "hub.List"),
+        MenuElement("/admin/namedSlices/add", "hub.Create")
+      )
+    )
+  )
 
   override def mainMenuEntries(configuration: OrganizationConfiguration, lang: String): Seq[MainMenuEntry] = {
     val slices = NamedSlice.dao(configuration.orgId).findAllPublished
